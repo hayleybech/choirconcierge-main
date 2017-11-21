fbdc24b5f7b19ded752898aeafe4eb40333b4d73
Unknown <haydenbech@gmail.com>
2017-11-21 18:42:08 +1000

    Add export action
    Add route and link. Start building export function.

--- app/Http/Controllers/SingersController.php
+++ app/Http/Controllers/SingersController.php
@@ -93,4 +93,73 @@ class SingersController extends Controller
 		return redirect('/singers')->with(['status' => 'The singer\'s fee status has been saved. ', 'Response' => $Response]);
 
 	}
+	
+	
+	// https://stackoverflow.com/questions/26146719/use-laravel-to-download-table-as-csv/27596496#27596496
+	public function export() {
+		$headers = [
+				'Cache-Control'       => 'must-revalidate, post-check=0, pre-check=0'
+			,   'Content-type'        => 'text/csv'
+			,   'Content-Disposition' => 'attachment; filename=galleries.csv'
+			,   'Expires'             => '0'
+			,   'Pragma'              => 'public'
+		];
+
+		//$list = Singer::all()->toArray();
+		
+		// Get subscribers
+		$Drip = new Drip('nsef8o9sjpmfake3czoq', '9922956');
+		$Response = $Drip->get('subscribers');
+		$singers = $Response->subscribers; 
+		
+
+		# add headers for each column in the CSV download
+		//array_unshift($list, array_keys($list[0]));
+
+	   //$callback = function() use ($list) 
+	   $callback = function() use ($singers) 
+		{
+			$FH = fopen('php://output', 'w');
+			foreach ($singers as $singer) { 
+				
+				$names = explode(' ', $singer['custom_fields']['name']);
+				
+				//fputcsv($FH, $row);
+				fputcsv($FH, array( 
+					'Login name'	=> singer['custom_fields']['name'],
+					'Email'			=> singer['email'],
+					'Can Log In'	=> true,
+					'Roles'			=> 'Member',
+					'First Name'	=> $names[0],
+					//'Last Name' 	=> $names[1],
+					'Nickname'  	=> '',
+					'Street'		=> '',
+					'Additional'	=> '',
+					'City'			=> '',
+					'Province'		=> '',
+					'Postal code'	=> '',
+					'Country'	 	=> '',
+					'Mobile phone'	=> '',
+					'Home phone'	=> '',
+					'Work phone'	=> '',
+					'Birthday'		=> '',
+					'Notes'			=> '',
+					'Voice Part'	=> singer['custom_fields']['Voice Part'],
+					'Member ID'		=> '',
+					'Skills'		=> '',
+					'Member Since'	=> '',
+					'Dues paid until'	=> '',
+					'Voice type'	=> '',
+					'Height'		=> '',
+					'Parent'		=> '',
+					'Spouse name'	=> '',
+					'Spouse birthday'	=> '',
+					'Anniversary'	=> '',
+				) );
+			}
+			fclose($FH);
+		};
+
+		return response()->stream($callback, 200, $headers);
+	}
 }
