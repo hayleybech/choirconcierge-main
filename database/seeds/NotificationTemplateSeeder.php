01456bdb233db958f4662770bf6557ca67982df0
Hayden <haydenbech@gmail.com>
2018-10-29 14:31:06 +1000

    Fixed incorrect indentation on seeded message content.

--- database/seeds/NotificationTemplateSeeder.php
+++ database/seeds/NotificationTemplateSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Carbon\Carbon;
 
 class NotificationTemplateSeeder extends Seeder
 {
@@ -16,27 +17,27 @@ class NotificationTemplateSeeder extends Seeder
 		// Create some dummy templates
 		DB::table('notification_templates')->insert([
 			[
-			'task_id'		=> 1,
-			'subject'		=> 'Singer Completed Member Profile',
-			'recipients'	=> 'user:1',
-			'body'			=> 
-				'Hi %%user.name%%,
-				A member profile has been completed for %%singer.name%%. ',
-			'delay'			=> 0,
+            'task_id'		=> 1,
+            'subject'		=> 'Singer Completed Member Profile',
+            'recipients'	=> 'user:1',
+            'body'			=>
+"Hi %%user.name%%,
+A member profile has been completed for %%singer.name%%. ",
+            'delay'			=> 0,
             'created_at'        => Carbon::now(),
             'updated_at'        => Carbon::now(),
 			],
 			[
-			'task_id'		=> 1,
-			'subject'		=> 'Welcome to The Blenders!',
-			'recipients'	=> 'singer:1',
-			'body'			=> 
-				'Hi %%singer.name%%,
-				Welcome to The Blenders! You have 4 weeks to learn your audition songs. 
-				You can download your songs [here].
-				Regards,
-				The Blenders',
-			'delay'			=> 0,
+            'task_id'		=> 1,
+            'subject'		=> 'Welcome to The Blenders!',
+            'recipients'	=> 'singer:1',
+            'body'			=>
+"Hi %%singer.name%%,
+Welcome to The Blenders! You have 4 weeks to learn your audition songs. 
+You can download your songs [here].
+Regards,
+The Blenders",
+            'delay'			=> 0,
             'created_at'        => Carbon::now(),
             'updated_at'        => Carbon::now(),
 			],
