fb9b23b66030773a32200f256417cb6d2f5baa6f
Hayden <haydenbech@gmail.com>
2018-11-17 10:27:27 +1000

    Fix nullable rollbacks with null values

--- database/migrations/2018_11_03_074916_modify_profiles_allow_nulls.php
+++ database/migrations/2018_11_03_074916_modify_profiles_allow_nulls.php
@@ -32,15 +32,25 @@ class ModifyProfilesAllowNulls extends Migration
      */
     public function down()
     {
+        // Need to replace null values before disallowing null
+        DB::statement('UPDATE `profiles` SET `dob` = CURRENT_TIMESTAMP WHERE `dob` IS NULL;');
+        DB::statement('UPDATE `profiles` SET `phone` = "" WHERE `phone` IS NULL;');
+        DB::statement('UPDATE `profiles` SET `ice_name` = "" WHERE `ice_name` IS NULL;');
+        DB::statement('UPDATE `profiles` SET `ice_phone` = "" WHERE `ice_phone` IS NULL;');
+        DB::statement('UPDATE `profiles` SET `reason_for_joining` = "" WHERE `reason_for_joining` IS NULL;');
+        DB::statement('UPDATE `profiles` SET `referrer` = "" WHERE `referrer` IS NULL;');
+        DB::statement('UPDATE `profiles` SET `profession` = "" WHERE `profession` IS NULL;');
+        DB::statement('UPDATE `profiles` SET `skills` = "" WHERE `skills` IS NULL;');
+
         Schema::table('profiles', function (Blueprint $table) {
-            $table->date('dob')->nullable(false)->change();
-            $table->string('phone')->nullable(false)->change();
-            $table->string('ice_name')->nullable(false)->change();
-            $table->string('ice_phone')->nullable(false)->change();
-            $table->string('reason_for_joining')->nullable(false)->change();
-            $table->string('referrer')->nullable(false)->change();
-            $table->string('profession')->nullable(false)->change();
-            $table->string('skills')->nullable(false)->change();
+            $table->date('dob')->useCurrent()->nullable(false)->change();
+            $table->string('phone')->default('')->nullable(false)->change();
+            $table->string('ice_name')->default('')->nullable(false)->change();
+            $table->string('ice_phone')->default('')->nullable(false)->change();
+            $table->string('reason_for_joining')->default('')->nullable(false)->change();
+            $table->string('referrer')->default('')->nullable(false)->change();
+            $table->string('profession')->default('')->nullable(false)->change();
+            $table->string('skills')->default('')->nullable(false)->change();
         });
     }
 }
--- database/migrations/2018_11_03_075812_modify_placements_allow_nulls.php
+++ database/migrations/2018_11_03_075812_modify_placements_allow_nulls.php
@@ -32,15 +32,25 @@ class ModifyPlacementsAllowNulls extends Migration
      */
     public function down()
     {
+        // Need to replace null values before disallowing null
+        DB::statement('UPDATE `placements` SET `experience` = "" WHERE `experience` IS NULL;');
+        DB::statement('UPDATE `placements` SET `instruments` = "" WHERE `instruments` IS NULL;');
+        DB::statement('UPDATE `placements` SET `skill_pitch` = 0 WHERE `skill_pitch` IS NULL;');
+        DB::statement('UPDATE `placements` SET `skill_harmony` = 0 WHERE `skill_harmony` IS NULL;');
+        DB::statement('UPDATE `placements` SET `skill_performance` = 0 WHERE `skill_performance` IS NULL;');
+        DB::statement('UPDATE `placements` SET `skill_sightreading` = 0 WHERE `skill_sightreading` IS NULL;');
+        DB::statement('UPDATE `placements` SET `voice_tone` = 0 WHERE `voice_tone` IS NULL;');
+        DB::statement('UPDATE `placements` SET `voice_part` = "" WHERE `voice_part` IS NULL;');
+
         Schema::table('placements', function (Blueprint $table) {
-            $table->string('experience')->nullable(false)->change();
-            $table->string('instruments')->nullable(false)->change();
-            $table->integer('skill_pitch')->nullable(false)->change();
-            $table->integer('skill_harmony')->nullable(false)->change();
-            $table->integer('skill_performance')->nullable(false)->change();
-            $table->integer('skill_sightreading')->nullable(false)->change();
-            $table->integer('voice_tone')->nullable(false)->change();
-            $table->string('voice_part')->nullable(false)->change();
+            $table->string('experience')->default('')->nullable(false)->change();
+            $table->string('instruments')->default('')->nullable(false)->change();
+            $table->integer('skill_pitch')->default(0)->nullable(false)->change();
+            $table->integer('skill_harmony')->default(0)->nullable(false)->change();
+            $table->integer('skill_performance')->default(0)->nullable(false)->change();
+            $table->integer('skill_sightreading')->default(0)->nullable(false)->change();
+            $table->integer('voice_tone')->default(0)->nullable(false)->change();
+            $table->string('voice_part')->default('')->nullable(false)->change();
         });
     }
 }
