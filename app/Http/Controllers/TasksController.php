ca0344b5823520c137694f34b7baa0097d6f113a
Unknown <haydenbech@gmail.com>
2019-12-14 09:55:59 +1000

    Fix Model not found

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -3,12 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Task;
 
 class TasksController extends Controller
 {
     public function index()
     {
-        $tasks = App\Task::all();
+        $tasks = Task::all();
 
         return view('tasks.tasks', compact('tasks'));
     }
