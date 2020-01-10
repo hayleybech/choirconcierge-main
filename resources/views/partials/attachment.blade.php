de0e8ceab3605534e17505dd5dd8b6f139f9afc9
Unknown <haydenbech@gmail.com>
2020-01-10 16:33:03 +1000

    Added attachment download button

--- resources/views/partials/attachment.blade.php
+++ resources/views/partials/attachment.blade.php
@@ -12,7 +12,8 @@
         {{ $attachment->category->title }}
     </div>
     <div class="r-table__cell column--actions">
-        <a href="{{route( 'song.attachments.delete', ['song' => $attachment->song, 'attachment' => $attachment] )}}" class="btn btn-outline-danger btn-sm"><i class="fa fa-fw fa-trash"></i>Delete</a>
+        <a href="{{ $attachment->getDownloadUrl() }}" class="btn btn-primary btn-sm"><i class="fa fa-fw fa-download"></i>Download</a>
+        <a href="{{route( 'song.attachments.delete', ['song' => $attachment->song, 'attachment' => $attachment] )}}" class="btn btn-outline-danger btn-sm ml-2"><i class="fa fa-fw fa-trash"></i>Delete</a>
     </div>
 </div>
 
