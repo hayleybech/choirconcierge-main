34a570f7085b6fd74d1a728393e603c619c15291
Unknown <haydenbech@gmail.com>
2020-03-23 18:42:49 +1000

    Revamped songs table

--- resources/views/songs/index_row.blade.php
+++ resources/views/songs/index_row.blade.php
@@ -1,30 +1,28 @@
-<div class="r-table__row">
-    <div class="r-table__cell column--mark">
+<div class="r-table__row row--song">
+    <div class="r-table__cell col--mark">
         <input type="checkbox" />
     </div>
-    <div class="r-table__cell column--title">
+    <div class="r-table__cell song-col--title">
         <a class="item-title" href="{{route('songs.show', ['song' => $song])}}">
         {{ ( isset($song->title) ) ? $song->title : 'Title Unknown' }}
         </a>
     </div>
-    <div class="r-table__cell column--status">
-        {{ $song->status->title }}
+    <div class="r-table__cell song-col--status">
+        <span class="badge badge-dark">{{ $song->status->title }}</span>
     </div>
-    <div class="r-table__cell column--category">
-    <ul class="list-inline">
-    @foreach( $song->categories as $cat )
-        <li class="list-inline-item">{{ $cat->title }}</li>
-    @endforeach
-    </ul>
+    <div class="r-table__cell song-col--category">
+        @foreach( $song->categories as $cat )
+        <span class="badge badge-secondary mr-1">{{ $cat->title }}</span>
+        @endforeach
     </div>
-    <div class="r-table__cell column--pitch">
+    <div class="r-table__cell song-col--pitch">
         {{ $song->pitch }}
     </div>
-    <div class="r-table__cell column--created">
+    <div class="r-table__cell song-col--created">
         {{ $song->created_at->diffForHumans() }}
     </div>
-    <div class="r-table__cell column--actions">
-        <a href="{{route( 'song.delete', ['song' => $song] )}}" class="link-confirm btn btn-link text-danger btn-sm ml-2"><i class="fa fa-fw fa-times"></i></a>
+    <div class="r-table__cell col--delete">
+        <a href="{{route( 'song.delete', ['song' => $song] )}}" class="link-confirm text-danger"><i class="fa fa-fw fa-times"></i></a>
     </div>
 
 </div>
