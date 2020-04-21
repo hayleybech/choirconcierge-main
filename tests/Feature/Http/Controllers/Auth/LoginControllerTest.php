dd1e68972dfffc74375f53f639f045ea65e6dc20
Hayden Bech <haydenbech@gfmail.com>
2020-04-21 18:48:14 +1000

    Created LoginControllerTest

---
+++ tests/Feature/Http/Controllers/Auth/LoginControllerTest.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers\Auth;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class LoginControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function login_displays_the_login_form()
+    {
+        $response = $this->get(route('login'));
+
+        $response->assertStatus(200);
+        $response->assertViewIs('auth.login');
+    }
+
+    /** @test */
+    public function invalid_login_displays_validation_errors()
+    {
+        $response = $this->post('/login', []);
+
+        $response->assertStatus(302);
+        $response->assertSessionHasErrors('email');
+    }
+
+    /** @test */
+    public function login_authenticates_and_redirects_user()
+    {
+        $user = factory(User::class)->create();
+
+        $response = $this->post(route('login'), [
+            'email' => $user->email,
+            'password' => 'password',
+        ]);
+
+        $response->assertRedirect('/dash');
+        $this->assertAuthenticatedAs($user);
+    }
+
+    /** @test */
+    public function logout_deauthenticates_and_redirects_user()
+    {
+        // login
+        $user = factory(User::class)->create();
+
+        $response = $this->post(route('login'), [
+            'email' => $user->email,
+            'password' => 'password',
+        ]);
+
+        // logout
+        $response = $this->post(route('logout'));
+
+        $response->assertRedirect('/');
+        $this->assertGuest();
+    }
+}
