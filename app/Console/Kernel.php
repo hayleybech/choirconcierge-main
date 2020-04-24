f8c85028c6883ac43656a5a395337be142bf8a52
Hayden Bech <haydenbech@gfmail.com>
2020-04-24 12:33:11 +1000

    Make the queue dirty cron fix disabled by default

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,36 +28,44 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')
         //          ->hourly();
 
-        // Allow cron job for Laravel queues on shared hosting
-        // https://papertank.com/blog/903/setup-laravel-queue-on-shared-hosting/
-        // https://gist.github.com/davidrushton/b7229df4c73372402fc1#file-kernel-php-L29
-        $path = base_path();
-        $schedule->call(function() use($path) {
-            if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
-                // just repeatedly exec on Windows
-                $command = PHP_BINARY . ' ' . $path . '/artisan queue:work --tries=3 > /dev/null & echo $!';
-                $number = exec($command);
-            } else {
-                // check process on Linux
-                if (file_exists($path . '/queue.pid')) {
-                    $pid = file_get_contents($path . '/queue.pid');
-                    $result = exec("ps -p $pid --no-heading | awk '{print $1}'");
-                    echo 'Checking queue worker';
-                    $run = $result === '';
-                } else {
-                    $run = true;
-                }
-                if($run) {
-                    //$command = '/usr/bin/php -c ' . $path .'/php.ini ' . $path . '/artisan queue:work --tries=3 > /dev/null & echo $!';
-                    $command = PHP_BINARY . ' ' . $path . '/artisan queue:work --tries=3 --stop-when-empty > /dev/null & echo $!';
+        if( config('queue.cron_fix') ){
+
+            /*
+             * The Dirty Cron Queue Fix
+             *
+             * Allow cron job for Laravel queues on shared hosting
+             *
+             * Only god knows how a QUEUED job could successfully keep the QUEUE listener running.
+             *
+             * @see https://papertank.com/blog/903/setup-laravel-queue-on-shared-hosting/
+             * @see https://gist.github.com/davidrushton/b7229df4c73372402fc1#file-kernel-php-L29
+             */
+            $path = base_path();
+            $schedule->call(function() use($path) {
+                if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+                    // just repeatedly exec on Windows
+                    $command = PHP_BINARY . ' ' . $path . '/artisan queue:work --tries=3 > /dev/null & echo $!';
                     $number = exec($command);
-                    echo 'Running queue worker';
-                    file_put_contents($path . '/queue.pid', $number);
+                } else {
+                    // check process on Linux
+                    if (file_exists($path . '/queue.pid')) {
+                        $pid = file_get_contents($path . '/queue.pid');
+                        $result = exec("ps -p $pid --no-heading | awk '{print $1}'");
+                        echo 'Checking queue worker';
+                        $run = $result === '';
+                    } else {
+                        $run = true;
+                    }
+                    if($run) {
+                        //$command = '/usr/bin/php -c ' . $path .'/php.ini ' . $path . '/artisan queue:work --tries=3 > /dev/null & echo $!';
+                        $command = PHP_BINARY . ' ' . $path . '/artisan queue:work --tries=3 --stop-when-empty > /dev/null & echo $!';
+                        $number = exec($command);
+                        echo 'Running queue worker';
+                        file_put_contents($path . '/queue.pid', $number);
+                    }
                 }
-            }
-
-
-        })->name('monitor_queue_listener')->everyMinute();
+            })->name('monitor_queue_listener')->everyMinute();
+        }
 
 
         $schedule->job(new ProcessGroupMailbox)->everyFiveMinutes();
@@ -73,5 +81,10 @@ class Kernel extends ConsoleKernel
         $this->load(__DIR__.'/Commands');
 
         require base_path('routes/console.php');
+    }
+
+    private function run_dirty_cron_queue_fix()
+    {
+
     }
 }
