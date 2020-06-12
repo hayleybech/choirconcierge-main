3a4b2119d171a31df0f2013ae6565b08fbe04cbc
Hayden Bech <haydenbech@gfmail.com>
2020-06-12 22:03:23 +1000

    Added a favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,6 +14,7 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+	<link rel="shortcut icon" href="/img/favicon.png">
 
 	<link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400&display=swap" rel="stylesheet">
 
