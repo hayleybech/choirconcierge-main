4fbcd1f946c655a1b9ffa9702eb7c93229f85ee7
Hayden Bech <haydenbech@gfmail.com>
2020-06-13 19:00:04 +1000

    Fix fatal error when task is empty in a notification template
    ... Not saying it should ever be empty but there we are, crashing nonetheless.

--- resources/views/notification-templates/index_row.blade.php
+++ resources/views/notification-templates/index_row.blade.php
@@ -1,5 +1,5 @@
 <tr>
-	<th scope="row">{{ $template->task->name }}</th>
+	<th scope="row">{{ optional($template->task)->name }}</th>
 	<td>{{ $template->subject }}</td>
 	<td>{{ $template->recipients }}</td>
 	<td>{!! $template->body_rendered !!}</td>
