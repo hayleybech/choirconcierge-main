946f924f4a0c1657f35b92056e7f9607c18ca77d
Hayden Bech <haydenbech@gfmail.com>
2020-06-15 15:58:00 +1000

    Added Cancel to Create/Edit Event

--- resources/views/events/edit.blade.php
+++ resources/views/events/edit.blade.php
@@ -111,7 +111,12 @@
         </div>
 
         <div class="card-footer">
-            {{ Form::submit('Save', array( 'class' => 'btn btn-primary' )) }}
+            <button type="submit" class="btn btn-primary">
+                <i class="fa fa-fw fa-check"></i> Save
+            </button>
+            <a href="{{ route('events.show', [$event]) }}" class="btn btn-outline-secondary">
+                <i class="fa fa-fw fa-times"></i> Cancel
+            </a>
         </div>
 
     </div>
