f0d422c51aff57072020a4d64d5dcaaa7ad52a63
Hayden Bech <haydenbech@gfmail.com>
2020-06-15 17:57:29 +1000

    Added Breadcrumbs for welcome, dash, Singers

--- resources/views/layouts/page.blade.php
+++ resources/views/layouts/page.blade.php
@@ -4,7 +4,11 @@
 
     <div class="page-header jumbotron jumbotron-fluid bg-light bg-gradient-blue-purple text-white">
         <div class="container">
+
             <h2 class="display-4">@yield('page-title') @yield('page-action')</h2>
+
+            {{ Breadcrumbs::render() }}
+
             <p class="lead">
                 @yield('page-lead')
             </p>
--- routes/breadcrumbs.php
+++ routes/breadcrumbs.php
@@ -1,1 +1,42 @@
 <?php
+
+/**
+ * WELCOME
+ */
+Breadcrumbs::for('welcome', static function($trail){
+    $trail->push('Welcome', route('menu'));
+});
+
+/**
+ * DASHBOARD
+ */
+Breadcrumbs::for('dash', static function($trail){
+    $trail->push('Dashboard', route('dash'));
+});
+
+/**
+ * SINGERS
+ */
+
+// Singers
+Breadcrumbs::for('singers.index', static function ($trail) {
+    $trail->push('Singers', route('singers.index'));
+});
+
+// Singers > [Singer]
+Breadcrumbs::for('singers.show', static function ($trail, $singer) {
+    $trail->parent('singers.index');
+    $trail->push($singer->name, route('singers.show', $singer));
+});
+
+// Singers > [Singer] > Edit
+Breadcrumbs::for('singers.edit', static function ($trail, $singer) {
+    $trail->parent('singers.show', $singer);
+    $trail->push('Edit', route('singers.edit', $singer));
+});
+
+// Singers > Create
+Breadcrumbs::for('singers.create', static function ($trail) {
+    $trail->parent('singers.index');
+    $trail->push('Create', route('singers.create'));
+});
