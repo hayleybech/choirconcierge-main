762f038b474b5cf406daa498a7dd0e953166ce25
Hayden Bech <haydenbech@gfmail.com>
2020-06-15 17:24:44 +1000

    Added Cancel to Create/Edit MailingList

--- resources/views/groups/edit.blade.php
+++ resources/views/groups/edit.blade.php
@@ -100,7 +100,12 @@
         </div>
 
         <div class="card-footer">
-            {{ Form::submit('Save', array( 'class' => 'btn btn-primary' )) }}
+            <button type="submit" class="btn btn-primary">
+                <i class="fa fa-fw fa-check"></i> Save
+            </button>
+            <a href="{{ route('groups.show', [$group]) }}" class="btn btn-outline-secondary">
+                <i class="fa fa-fw fa-times"></i> Cancel
+            </a>
         </div>
 
     </div>
