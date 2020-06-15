885fe305e17825ae5c05dc895747487671252050
Hayden Bech <haydenbech@gfmail.com>
2020-06-15 18:45:36 +1000

    Made breadcrumbs more subtle

--- resources/views/layouts/page.blade.php
+++ resources/views/layouts/page.blade.php
@@ -5,9 +5,9 @@
     <div class="page-header jumbotron jumbotron-fluid bg-light bg-gradient-blue-purple text-white">
         <div class="container">
 
-            <h2 class="display-4">@yield('page-title') @yield('page-action')</h2>
-
             {{ Breadcrumbs::render() }}
+
+            <h2 class="display-4">@yield('page-title') @yield('page-action')</h2>
 
             <p class="lead">
                 @yield('page-lead')
