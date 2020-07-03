f1ba20a90b7dfb85cc5a8b461280e21b2ed7881d
Hayden Bech <haydenbech@gfmail.com>
2020-07-03 13:04:15 +1000

    Updated branding for voice parts

--- resources/views/voice-parts/index.blade.php
+++ resources/views/voice-parts/index.blade.php
@@ -2,17 +2,16 @@
 
 @section('title', 'Voice Parts')
 @section('page-title')
-<i class="fal fa-users-class fa-fw"></i> Voice Parts
+	<i class="fal fa-users-class fa-fw"></i> Voice Parts
+	<a href="{{ route( 'voice-parts.create' ) }}" class="btn btn-add btn-sm btn-primary ml-2"><i class="fa fa-fw fa-plus"></i> Add New</a>
 @endsection
 @section('page-action')
-	<a href="{{ route( 'voice-parts.create' ) }}" class="btn btn-add btn-sm btn-light"><i class="fa fa-fw fa-plus"></i> Add New</a>
 @endsection
 
 @section('page-lead', 'On this page you can tweak the names of your voice parts, and add/remove them when necessary. NOTE: Changing an existing part will affect singers in that part.')
 
 @section('page-content')
-
-	<div class="card bg-light">
+	<div class="card">
 		<h3 class="card-header h4">Voice Part List</h3>
 
 		<div class="list-group-flush">
@@ -24,5 +23,4 @@
 			@endforeach
 		</div>
 	</div>
-
 @endsection
