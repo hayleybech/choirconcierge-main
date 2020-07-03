1e460df1880e4c79a08de8c4044ba9da8bdafb9d
Hayden Bech <haydenbech@gfmail.com>
2020-07-03 13:20:15 +1000

    Updated branding on events index

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -2,44 +2,41 @@
 
 @section('title', 'Events')
 @section('page-title')
-<i class="fal fa-fw fa-calendar"></i> Events
+    <i class="fal fa-fw fa-calendar"></i> Events
+    @if(Auth::user()->hasRole('Music Team'))
+        <a href="{{route( 'events.create' )}}" class="btn btn-add btn-sm btn-primary ml-2"><i class="fa fa-fw fa-calendar-plus"></i> Add New</a>
+    @endif
 @endsection
 
 @section('page-action')
-    @if(Auth::user()->hasRole('Music Team'))
-    <a href="{{route( 'events.create' )}}" class="btn btn-add btn-sm btn-light"><i class="fa fa-fw fa-calendar-plus"></i> Add New</a>
-    @endif
+    <?php
+    use App\Models\Event;
+    $filters_class = Event::hasActiveFilters() ? 'btn-primary' : 'btn-light';
+    ?>
+    <a class="btn btn-sm {{ $filters_class }}" data-toggle="collapse" href="#filters" role="button" aria-expanded="false" aria-controls="filters"><i class="fa fa-filter"></i> Filter</a>
+
 @endsection
 
 @section('page-content')
 
-    <div class="card bg-light mb-5">
-        <h3 class="card-header h4">Upcoming Events</h3>
+    <div class="d-flex justify-content-end">
+        <div class="collapse mt-2" id="filters">
 
-        <div class="card-body">
+            <form method="get" class="form-inline mb-0">
+                @each('partials.filter', $filters, 'filter')
 
-            <?php
-            use App\Models\Event;
-            $filters_class = Event::hasActiveFilters() ? 'btn-primary' : 'btn-outline-secondary';
-            ?>
-            <a class="btn btn-sm {{ $filters_class }}" data-toggle="collapse" href="#filters" role="button" aria-expanded="false" aria-controls="filters"><i class="fa fa-filter"></i> Filter</a>
-
-            <div class="collapse mt-2" id="filters">
-
-                <form method="get" class="form-inline mb-0">
-                    @each('partials.filter', $filters, 'filter')
-
-                    <div class="input-group input-group-sm mb-2 mr-2">
-                        <div class="btn-group" role="group" aria-label="Basic example">
-                            <button class="btn btn-outline-success btn-sm"><i class="fa fa-check"></i> Apply</button>
-                            <a href="{{ route('events.index') }}" class="btn btn-outline-danger btn-sm"><i class="fa fa-trash"></i> Clear</a>
-                        </div>
+                <div class="input-group input-group-sm mb-2 mr-2">
+                    <div class="btn-group" role="group" aria-label="Basic example">
+                        <button class="btn btn-outline-success btn-sm"><i class="fa fa-check"></i> Apply</button>
+                        <a href="{{ route('events.index') }}" class="btn btn-outline-danger btn-sm"><i class="fa fa-trash"></i> Clear</a>
                     </div>
-                </form>
-
-            </div>
-
+                </div>
+            </form>
         </div>
+    </div>
+
+    <div class="card mb-5">
+        <h3 class="card-header h4">Upcoming Events</h3>
 
         <div class="r-table r-table--card-view-mobile">
             <div class="r-table__thead">
@@ -64,32 +61,8 @@
 
     </div>
 
-    <div class="card bg-light mb-5">
+    <div class="card mb-5">
         <h3 class="card-header h4">Past Events</h3>
-
-        <div class="card-body">
-
-            <?php
-            $filters_class = Event::hasActiveFilters() ? 'btn-primary' : 'btn-outline-secondary';
-            ?>
-            <a class="btn btn-sm {{ $filters_class }}" data-toggle="collapse" href="#filters" role="button" aria-expanded="false" aria-controls="filters"><i class="fa fa-filter"></i> Filter</a>
-
-            <div class="collapse mt-2" id="filters">
-
-                <form method="get" class="form-inline mb-0">
-                    @each('partials.filter', $filters, 'filter')
-
-                    <div class="input-group input-group-sm mb-2 mr-2">
-                        <div class="btn-group" role="group" aria-label="Basic example">
-                            <button class="btn btn-outline-success btn-sm"><i class="fa fa-check"></i> Apply</button>
-                            <a href="{{ route('events.index') }}" class="btn btn-outline-danger btn-sm"><i class="fa fa-trash"></i> Clear</a>
-                        </div>
-                    </div>
-                </form>
-
-            </div>
-
-        </div>
 
         <div class="r-table r-table--card-view-mobile">
             <div class="r-table__thead">
