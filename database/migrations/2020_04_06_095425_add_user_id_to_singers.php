a01945aaf3ef7302c528aacf885c643e16c159a2
Hayden Bech <haydenbech@gfmail.com>
2020-07-29 14:17:48 +1000

    Remove ALL eloquent from migrations

--- database/migrations/2020_04_06_095425_add_user_id_to_singers.php
+++ database/migrations/2020_04_06_095425_add_user_id_to_singers.php
@@ -4,6 +4,7 @@ use App\Models\Singer;
 use App\Models\SingerCategory;
 use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Query\Builder;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Str;
@@ -23,45 +24,15 @@ class AddUserIdToSingers extends Migration
             //$table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
 
-        // Insert users missing for singers
-        $singers_wo_users = Singer::doesntHave('user')->get();
+        $this->sync_existing_users_and_singers();
 
-        /** @var Singer $singer */
-        foreach ($singers_wo_users as $singer) {
-            $user = new User();
-            $user->name = $singer->name;
-            $user->email = $singer->email;
-            $user->password = Str::random(10); // Require singers to create a password later.
-            $user->save();
+        $this->insert_users_missing_for_singers();
 
-            $singer->user()->associate($user);
-            $singer->save();
-        }
-
-        // Insert singers missing for users
-        $users_wo_singers = User::doesntHave('singer')->get();
-        /** @var User $user */
-        foreach($users_wo_singers as $user) {
-            $singer = new Singer();
-            $singer->name = $user->name;
-            $singer->email = $user->email;
-            $singer->onboarding_enabled = false;
-            $singer->user()->associate($user);
-
-            // Attach to category
-            $MEMBERS_CAT_ID = 3;
-            $category_id = $MEMBERS_CAT_ID;
-            $category = SingerCategory::find($category_id);
-            $singer->category()->associate($category);
-
-            $singer->save();
-
-            $user->save();
-        }
+        $this->insert_singers_missing_for_users();
     }
 
     /**
-     * Reverse the migrations.
+     * Reverse the migrations..
      *
      * @return void
      */
@@ -71,4 +42,77 @@ class AddUserIdToSingers extends Migration
             $table->dropColumn('onboarding_enabled');
         });
     }
+
+    /**
+     * Finds people who were added as a singer AND a user
+     * (when those were completely separate features)
+     * then connects the two records together.
+     */
+    public function sync_existing_users_and_singers(): void {
+        $people_to_sync = DB::table('singers')
+            ->join('users', 'users.email', '=', 'singers.email')
+            ->select('users.id as user_id', 'singers.id as singer_id')
+            ->get();
+
+        foreach($people_to_sync as $person)
+        {
+            DB::table('singers')->where('id', $person->singer_id)
+                ->update(['user_id' => $person->user_id]);
+        }
+    }
+
+    public function insert_users_missing_for_singers(): void {
+        $singers_wo_users = DB::table('singers')
+            ->where('user_id', '=', null)
+            ->get();
+
+        /** @var Object $singer */
+        foreach ($singers_wo_users as $singer) {
+
+            $user_id = DB::table('users')->insertGetId(
+                $user = [
+                    'name'      => $singer->name,
+                    'email'     => $singer->email,
+                    'password'  => Str::random(10), // Require singers to create a password later.
+                    'created_at'=> now(),
+                    'updated_at'=> now(),
+                ]
+            );
+
+            DB::table('singers')->where('id', $singer->id)
+                ->update(['user_id' => $user_id]);
+        }
+    }
+
+    public function insert_singers_missing_for_users(): void {
+        $users_wo_singers = DB::table('users')
+            ->whereNotExists(static function(Builder $query){
+                $prefix = Config::get('database.connections.mysql.prefix');
+
+                $query->select(DB::raw(1))
+                    ->from('singers')
+                    ->whereRaw($prefix.'singers.user_id = '.$prefix.'users.id');
+            })
+        ->get();
+
+        $MEMBERS_CAT_ID = 3;
+
+        $singers_to_insert = [];
+        /** @var User $user */
+        foreach($users_wo_singers as $user) {
+
+            $singers_to_insert[] = [
+                'name'                  => $user->name,
+                'email'                 => $user->email,
+                'onboarding_enabled'    => false,
+                'user_id'               => $user->id,
+                'singer_category_id'    => $MEMBERS_CAT_ID,
+                'created_at'            => now(),
+                'updated_at'            => now(),
+            ];
+        }
+        DB::table('singers')
+            ->insert($singers_to_insert);
+    }
+
 }
