706bb30b975027582dced9b138cc1017a1d81418
Hayden Bech <haydenbech@gfmail.com>
2020-08-17 14:13:02 +1000

    Add spacing to sort icons

--- resources/views/events/table.blade.php
+++ resources/views/events/table.blade.php
@@ -1,11 +1,11 @@
 <table class="table card-table">
     <thead>
     <tr class="row--event">
-        <th class="col--title"><a href="{{ $sorts['title']['url'] }}">Title<i class="fa fas sort-{{ $sorts['title']['dir'] }} {{ ($sorts['title']['current'] ? 'sort-active' : 'sort-inactive' ) }}"></i></a></th>
-        <th class="col--type"><a href="{{ $sorts['type.title']['url'] }}">Type<i class="fa fas sort-{{ $sorts['type.title']['dir'] }} {{ ($sorts['type.title']['current'] ? 'sort-active' : 'sort-inactive' ) }}"></i></a></th>
+        <th class="col--title"><a href="{{ $sorts['title']['url'] }}">Title<i class="ml-1 fa fas sort-{{ $sorts['title']['dir'] }} {{ ($sorts['title']['current'] ? 'sort-active' : 'sort-inactive' ) }}"></i></a></th>
+        <th class="col--type"><a href="{{ $sorts['type.title']['url'] }}">Type<i class="ml-1 fa fas sort-{{ $sorts['type.title']['dir'] }} {{ ($sorts['type.title']['current'] ? 'sort-active' : 'sort-inactive' ) }}"></i></a></th>
         <th class="col--date">Event Date</th>
         <th class="col--location">Location</th>
-        <th class="col--created"><a href="{{ $sorts['created_at']['url'] }}">Created<i class="fa fas sort-{{ $sorts['created_at']['dir'] }} {{ ($sorts['created_at']['current'] ? 'sort-active' : 'sort-inactive' ) }}"></i></a></th>
+        <th class="col--created"><a href="{{ $sorts['created_at']['url'] }}">Created<i class="ml-1 fa fas sort-{{ $sorts['created_at']['dir'] }} {{ ($sorts['created_at']['current'] ? 'sort-active' : 'sort-inactive' ) }}"></i></a></th>
         <th class="col--delete"></th>
     </tr>
     </thead>
--- resources/views/singers/table.blade.php
+++ resources/views/singers/table.blade.php
@@ -1,9 +1,9 @@
 <table class="table card-table">
     <thead>
     <tr class="row--singer">
-        <th class="col--title"><a href="{{ $sorts['name']['url'] }}">Singer<i class="fa fas sort-{{ $sorts['name']['dir'] }} {{ ($sorts['name']['current'] ? 'sort-active' : 'sort-inactive' ) }}"></i></a></th>
-        <th class="col--part"><a href="{{ $sorts['voice_part']['url'] }}">Part<i class="fa fas sort-{{ $sorts['voice_part']['dir'] }} {{ ($sorts['voice_part']['current'] ? 'sort-active' : 'sort-inactive' ) }} "></i></a></th>
-        <th class="col--category"><a href="{{ $sorts['category.name']['url'] }}"><span class="status__title">Category</span><i class="fa fas sort-{{ $sorts['category.name']['dir'] }} {{ ($sorts['category.name']['current'] ? 'sort-active' : 'sort-inactive' ) }}"></i></a></th>
+        <th class="col--title"><a href="{{ $sorts['name']['url'] }}">Singer<i class="ml-1 fa fas sort-{{ $sorts['name']['dir'] }} {{ ($sorts['name']['current'] ? 'sort-active' : 'sort-inactive' ) }}"></i></a></th>
+        <th class="col--part"><a href="{{ $sorts['voice_part']['url'] }}">Part<i class="ml-1 fa fas sort-{{ $sorts['voice_part']['dir'] }} {{ ($sorts['voice_part']['current'] ? 'sort-active' : 'sort-inactive' ) }} "></i></a></th>
+        <th class="col--category"><a href="{{ $sorts['category.name']['url'] }}"><span class="status__title">Category</span><i class="ml-1 fa fas sort-{{ $sorts['category.name']['dir'] }} {{ ($sorts['category.name']['current'] ? 'sort-active' : 'sort-inactive' ) }}"></i></a></th>
         <th class="col--progress">Progress</th>
         <th class="col--actions">Actions</th>
         <th class="col--delete"></th>
