e3a4ed0a0a3b66924731960e9bf02c547c064f69
Hayden Bech <haydenbech@gfmail.com>
2020-09-07 15:55:58 +1000

    Fixed daterangepicker not loading

--- resources/views/events/edit.blade.php
+++ resources/views/events/edit.blade.php
@@ -129,6 +129,7 @@
     {{ Form::close() }}
 
     @push('scripts-footer-bottom')
+        <script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
         <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
         <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
         <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=<?= env('API_GOOGLE_KEY') ?>&libraries=places&callback=initMap" async defer></script>
