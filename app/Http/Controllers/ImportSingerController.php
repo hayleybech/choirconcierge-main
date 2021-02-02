666cba65f43e1f47de680a19d411dbb51132cded
Hayden Bech <haydenbech@gmail.com>
2021-02-02 11:47:20 +0800

    Only allow import for admins

--- app/Http/Controllers/ImportSingerController.php
+++ app/Http/Controllers/ImportSingerController.php
@@ -6,11 +6,15 @@ use App\Imports\SingersImport;
 use Excel;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ImportSingerController extends Controller
 {
     public function __invoke(Request $request): RedirectResponse
     {
+        if( ! optional(Auth::user())->hasRole('Admin')) {
+            abort(403);
+        }
         $request->validate([
             'import_csv' => 'required|file',
         ]);
