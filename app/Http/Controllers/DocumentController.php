320db9a18fbb3cb66fc63b1375fe956996a60d6a
Hayden Bech <haydenbech@gmail.com>
2021-02-02 13:19:23 +0800

    Removed unused DocumentController methods

--- app/Http/Controllers/DocumentController.php
+++ app/Http/Controllers/DocumentController.php
@@ -12,25 +12,6 @@ use Symfony\Component\HttpFoundation\BinaryFileResponse;
 
 class DocumentController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
 
     /**
      * Store a newly created resource in storage.
@@ -63,30 +44,6 @@ class DocumentController extends Controller
         return response()->download( $document->path );
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param Document $document
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Document $document)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request $request
-     * @param Document                  $document
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Document $document)
-    {
-        //
-    }
 
     /**
      * Remove the specified resource from storage.
