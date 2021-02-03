ae58562d83f340d6bdfae28390a582030f073575
Hayden Bech <haydenbech@gmail.com>
2021-02-03 12:07:32 +0800

    Use config not env for google api key

--- resources/views/events/show.blade.php
+++ resources/views/events/show.blade.php
@@ -100,7 +100,7 @@
                                 width="600"
                                 height="450"
                                 frameborder="0" style="border:0"
-                                src="https://www.google.com/maps/embed/v1/place?key=<?= env('API_GOOGLE_KEY') ?>&q=place_id:<?= urlencode($event->location_place_id)?>"
+                                src="https://www.google.com/maps/embed/v1/place?key=<?= config('services.google.key') ?>&q=place_id:<?= urlencode($event->location_place_id)?>"
                                 allowfullscreen>
                         </iframe>
                     </div>
