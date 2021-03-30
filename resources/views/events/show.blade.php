f92072bc9fafd89e8065e0695c5d4387cfbbe9a6
Hayden Bech <haydenbech@gmail.com>
2021-03-30 13:20:30 +0800

    Moved Google Map embed to a Vue component

--- resources/views/events/show.blade.php
+++ resources/views/events/show.blade.php
@@ -103,16 +103,7 @@
                         <span class="place-address">{{ $event->location_address }}</span>
                     </p>
 
-                    <div class="event-map google-maps">
-                        <iframe
-                                width="600"
-                                height="450"
-                                frameborder="0" style="border:0"
-                                src="https://www.google.com/maps/embed/v1/place?key=<?= config('services.google.key') ?>&q=place_id:<?= urlencode($event->location_place_id)?>"
-                                allowfullscreen>
-                        </iframe>
-                    </div>
-
+                    <google-map api-key="{{ config('services.google.key')  }}" place-id="{{ urlencode($event->location_place_id) }}"></google-map>
                 </div>
             </div>
 
