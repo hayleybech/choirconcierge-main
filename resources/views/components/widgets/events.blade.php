f75faa1b41c56c8f60c547361218d2ad5d293df8
Hayden Bech <haydenbech@gmail.com>
2021-04-02 21:04:07 +0800

    Highlight today's event and link to attendance

--- resources/views/components/widgets/events.blade.php
+++ resources/views/components/widgets/events.blade.php
@@ -6,11 +6,21 @@
         @forelse($events as $event)
             <div class="list-group-item">
                 <div>
-                    {{ $event->call_time->format('D, '.config('app.formats.date_md')) }} - <a href="{{ route('events.show', $event) }}">{{ $event->title }}</a>
+                    @if( $event->call_time->isToday() )
+                    <strong>Today</strong>
+                    @else
+                    {{ $event->call_time->format('D, '.config('app.formats.date_md')) }}
+                    @endif
+                     - <a href="{{ route('events.show', $event) }}">{{ $event->title }}</a>
                 </div>
                 <div>
                     <small><x-inline-rsvp :event="$event" :compact="true"></x-inline-rsvp></small>
                 </div>
+                @if( $event->call_time->isToday() )
+                @can('create', \App\Models\Attendance::class)
+                    <a href="{{ route('events.attendances.index', ['event' => $event]) }}" class="btn btn-secondary btn-sm"><i class="fas fa-fw fa-edit"></i> Record Attendance</a>
+                @endcan
+                @endif
             </div>
         @empty
             <div class="list-group-item">No events this month.</div>
