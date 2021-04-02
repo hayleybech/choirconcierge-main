81e98228a011276144c71d64ed0d219934becf65
Hayden Bech <haydenbech@gmail.com>
2021-04-02 21:18:37 +0800

    Wrap widgets in permissions checks

--- resources/views/dash.blade.php
+++ resources/views/dash.blade.php
@@ -10,15 +10,21 @@
 
     <div class="row">
         <div class="col-md-4">
+            @can('viewAny', \App\Models\Event::class)
             <x-widgets.events :events="$events"></x-widgets.events>
+            @endcan
         </div>
 
         <div class="col-md-4">
+            @can('viewAny', \App\Models\Song::class)
             <x-widgets.songs :songs="$songs"></x-widgets.songs>
+            @endcan
         </div>
 
         <div class="col-md-4">
+            @can('viewAny', \App\Models\Singer::class)
             <x-widgets.birthdays :birthdays="$birthdays" :empty-dobs="$empty_dobs"></x-widgets.birthdays>
+            @endcan
         </div>
     </div>
 
