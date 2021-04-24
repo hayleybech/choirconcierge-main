33e23d8a6c3f2b3f1e5f0cc359f6770993def823
Hayden Bech <haydenbech@gmail.com>
2021-04-24 16:28:04 +0800

    Added last login date

---
+++ app/Listeners/LogSuccessfulLogin.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Auth\Events\Login;
+
+class LogSuccessfulLogin
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  Login  $event
+     * @return void
+     */
+    public function handle(Login $event): void
+    {
+    	$event->user->timestamps = false; // No need to touch updated_at for this
+	    $event->user->last_login = date('Y-m-d H:i:s');
+	    $event->user->save();
+    }
+}
