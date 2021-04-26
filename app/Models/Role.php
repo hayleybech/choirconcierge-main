e79b49c40fb1f64b0c8910dcc573a1721a339f56
Hayden Bech <haydenbech@gmail.com>
2021-04-26 17:13:37 +0800

    Finish RoleControllerTest

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -2,11 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Collection;
 use Stancl\Tenancy\Database\Concerns\BelongsToTenant;
 
@@ -17,6 +19,8 @@ use Stancl\Tenancy\Database\Concerns\BelongsToTenant;
  * @property int $id
  * @property string $name
  * @property string[] $abilities
+ * @property Carbon $deleted_at
+ * @property int $tenant_id
  *
  * Relationships
  * @property Collection<User> $users
@@ -26,7 +30,7 @@ use Stancl\Tenancy\Database\Concerns\BelongsToTenant;
  */
 class Role extends Model
 {
-    use BelongsToTenant, SoftDeletes;
+    use BelongsToTenant, SoftDeletes, HasFactory;
 
     public $timestamps = false;
 
@@ -46,6 +50,24 @@ class Role extends Model
     protected $attributes = [
         'abilities' => '{}',
     ];
+
+	public const ALL_ABILITIES = [
+		'singers_view', 'singers_create', 'singers_update', 'singers_delete',
+		'singer_profiles_view', 'singer_profiles_create', 'singer_profiles_update',
+		'singer_placements_view', 'singer_placements_create', 'singer_placements_update',
+		'voice_parts_view', 'voice_parts_create', 'voice_parts_update', 'voice_parts_delete',
+		'roles_view', 'roles_create', 'roles_update', 'roles_delete',
+		'songs_view', 'songs_create', 'songs_update', 'songs_delete',
+		'events_view', 'events_create', 'events_update', 'events_delete',
+		'attendances_view', 'attendances_create', 'attendances_update', 'attendances_delete',
+		'rsvps_view',
+		'folders_view', 'folders_create', 'folders_update', 'folders_delete',
+		'documents_view', 'documents_create', 'documents_delete',
+		'riser_stacks_view', 'riser_stacks_create', 'riser_stacks_update', 'riser_stacks_delete',
+		'mailing_lists_view', 'mailing_lists_create', 'mailing_lists_update', 'mailing_lists_delete',
+		'tasks_view', 'tasks_create', 'tasks_update', 'tasks_delete',
+		'notifications_view', 'notifications_create', 'notifications_update', 'notifications_delete',
+	];
 
     /**
      * Get users with a certain role
