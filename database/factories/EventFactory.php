4452c36e9fb6c7e6b34505c5fa492b5cc8369790
Hayden Bech <haydenbech@gmail.com>
2021-04-26 19:00:48 +0800

    Finish EventControllerTest

--- database/factories/EventFactory.php
+++ database/factories/EventFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use App\Models\Event;
+use App\Models\EventType;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
@@ -18,13 +19,16 @@ class EventFactory extends Factory
         $end_time = (clone $start_time)->addHours(2);
 
         return [
-            'title' => $this->faker->sentence(6, true),
-            'call_time' => $call_time,
-            'start_date' => $start_time,
-            'end_date' => $end_time,
-            'location_name' => $this->faker->sentence(3, true),
-            'location_address' => $this->faker->address, // @todo Use random REAL address for map testing (https://github.com/nonsapiens/addressfactory)
-            'description' => $this->faker->optional()->sentence,
+            'title'                 => $this->faker->sentence(6, true),
+            'call_time'             => $call_time,
+            'start_date'            => $start_time,
+            'end_date'              => $end_time,
+            'location_name'         => $this->faker->sentence(3, true),
+            'location_address'      => $this->faker->address, // @todo Use random REAL address for map testing (https://github.com/nonsapiens/addressfactory)
+            'description'           => $this->faker->optional()->sentence,
+	        'type_id'               => EventType::where('title', 'Rehearsal')->value('id'),
+	        'created_at'            => now(),
+	        'updated_at'            => now(),
         ];
     }
 }
