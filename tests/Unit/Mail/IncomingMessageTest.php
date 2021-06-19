ba04b51eaffb6ac934b48d2a689708b14fba7d02
Hayden Bech <haydenbech@gmail.com>
2021-06-19 17:20:21 +0800

    Simplified tests

--- tests/Unit/Mail/IncomingMessageTest.php
+++ tests/Unit/Mail/IncomingMessageTest.php
@@ -21,58 +21,6 @@ class IncomingMessageTest extends TestCase
 
 	protected bool $tenancy = false;
 
-	/**
-	 * @test
-	 * @dataProvider recipientsToAcceptProvider
-	 */
-	public function resendToGroup($input): void
-	{
-		$this->markTestIncomplete('Fails for multiple recipients');
-
-		// Arrange
-		Mail::fake();
-
-		$tenant = Tenant::create(
-			'tenant1',
-			'Tenant One',
-			'Australia/Perth',
-		);
-		$tenant->domains()->create(['domain' => 'tenant1']);
-
-		$group_expected = UserGroup::create([
-			'title'     => 'Music Team',
-			'slug'      => 'music-team',
-			'list_type' => 'chat',
-			'tenant_id' => $tenant->id,
-		]);
-		$user1 = User::factory()->create([
-			'email'     => 'permitted@example.com',
-			'tenant_id' => $tenant->id,
-		]);
-		$user2 = User::factory()->create();
-		$group_expected->recipient_users()->attach([$user1->id, $user2->id]);
-
-		// @todo test with roles
-
-		$message = (new IncomingMessage())
-			->to($input['to'])
-			->cc($input['cc'])
-			->bcc($input['bcc'])
-			->from($input['from'])
-			->subject('Just a test');
-
-		// Act
-		$message->resendToGroups();
-
-		// Assert
-		Mail::assertSent(IncomingMessage::class, 2);
-		Mail::assertSent(IncomingMessage::class, static function($mail) use ($input){
-			$mail->build();
-			return $mail->hasFrom('music-team@tenant1.choirconcierge.test')
-				&& $mail->hasReplyTo($input['from']);
-		});
-	}
-
     /**
      * @test
      */
@@ -171,166 +119,224 @@ class IncomingMessageTest extends TestCase
         });
     }
 
-	/**
-	 * @test
-	 * @dataProvider recipientsToAcceptProvider
-	 * @dataProvider recipientsToRejectProvider
-	 */
-    public function getMatchingGroup($groups, $input, $should_match): void
+	/** @test */
+    public function getMatchingGroups_matches_one_group(): void
     {
     	// Arrange
-	    Tenant::create(
-	    	'tenant1',
-		    'Tenant One',
+	    $tenant = Tenant::create(
+	    	'test-tenant',
+		    'Test Tenant',
 		    'Australia/Perth',
 	    );
-        Tenant::create(
-            'tenant2',
-            'Tenant Two',
-            'Australia/Perth',
-        );
 
-	    $groups_expected = collect($groups)->map(fn($group) => Tenant::find($group['tenant'])
-            ->run(fn() => UserGroup::create([
-	        'title'     => $group['name'],
-            'slug'      => Str::slug($group['name']),
+	    $tenant->run(fn() => UserGroup::create([
+	        'title'     => 'Test Group',
+            'slug'      => 'test-group',
             'list_type' => 'chat',
-        ])));
+        ]));
 
     	$message = (new IncomingMessage())
-	        ->to($input['to'])
-	        ->cc($input['cc'])
-	        ->bcc($input['bcc'])
-	        ->from($input['from'])
+	        ->to('test-group@test-tenant.choirconcierge.test')
+	        ->from('sender@example.com')
 	        ->subject('Just a test');
 
     	// Act
         $groups_found = $message->getMatchingGroups()->flatten(1);
 
 	    // Assert
-        self::assertCount($should_match, $groups_found);
+        self::assertCount(1, $groups_found);
     }
 
-    public function recipientsToAcceptProvider(): array
+    /** @test */
+    public function getMatchingGroups_matches_groups_from_the_same_tenant(): void
     {
-        $default_groups = [
-            ['name' => 'Music Team', 'tenant' => 'tenant1'],
-        ];
-        $default_input = [
-            'to'     => 'anybody@example.com',
-            'cc'     => 'somebody@example.com',
-            'bcc'    => 'nobody@example.com',
-            'from'   => 'permitted@example.com',
-        ];
+        // Arrange
+        $tenant = Tenant::create(
+            'test-tenant',
+            'Test Tenant',
+            'Australia/Perth',
+        );
 
-    	return [
-			'Single TO' => [
-                'groups' => $default_groups,
-		   	    'input' => array_merge($default_input, [
-                    'to'     => 'music-team@tenant1.choirconcierge.test', // @todo test different subdomains
-		        ]),
-				1,
-			],
-			'Match single To in multiple TOs' => [
-                'groups' => $default_groups,
-                'input' => array_merge($default_input, [
-                    'to'     => [
-                        'skip@example.com',
-                        'music-team@tenant1.choirconcierge.test',
-                    ],
-                ]),
-                1
-			],
-            'Match multiple Tos' => [
-                'groups' => [
-                    ['name' => 'Music Team', 'tenant' => 'tenant1'],
-                    ['name' => 'Membership Team', 'tenant' => 'tenant1'],
-                ],
-                'input' => array_merge($default_input, [
-                    'to'     => [
-                        'skip@example.com',
-                        'music-team@tenant1.choirconcierge.test',
-                        'membership-team@tenant1.choirconcierge.test',
-                    ],
-                ]),
-                2
-            ],
-            'Match Tos from multiple tenants' => [
-                'groups' => [
-                    ['name' => 'Music Team', 'tenant' => 'tenant1'],
-                    ['name' => 'Music Team', 'tenant' => 'tenant2'],
-                ],
-                'input' => array_merge($default_input, [
-                    'to'     => [
-                        'skip@example.com',
-                        'music-team@tenant1.choirconcierge.test',
-                        'music-team@tenant2.choirconcierge.test',
-                    ],
-                ]),
-                2
-            ],
-		    'Single CC' => [
-                'groups' => $default_groups,
-			    'input' => array_merge($default_input, [
-				    'cc'     => 'music-team@tenant1.choirconcierge.test',
-			    ]),
-			    true
-		    ],
-		    'Multiple CCs' => [
-                'groups' => $default_groups,
-                'input' => array_merge($default_input, [
-                    'cc'     => [
-                        'skip@example.com',
-                        'music-team@tenant1.choirconcierge.test',
-                    ],
-                ]),
-			    true
-		    ],
-		    'Single BCC' => [
-                'groups' => $default_groups,
-                'input' => array_merge($default_input, [
-                    'bcc'     => 'music-team@tenant1.choirconcierge.test',
-                ]),
-			    true
-		    ],
-		    'Multiple BCCs' => [
-                'groups' => $default_groups,
-                'input' => array_merge($default_input, [
-				    'bcc'     => [
-                        'skip@example.com',
-                        'music-team@tenant1.choirconcierge.test',
-                    ],
-                ]),
-			    true
-		    ],
-	    ];
+        $tenant->run(function () {
+            UserGroup::create([
+                'title'     => 'Test Group 1',
+                'slug'      => 'test-group-1',
+                'list_type' => 'chat',
+            ]);
+            UserGroup::create([
+                'title'     => 'Test Group 2',
+                'slug'      => 'test-group-2',
+                'list_type' => 'chat',
+            ]);
+        });
+
+        $message = (new IncomingMessage())
+            ->to(['test-group-1@test-tenant@choirconcierge.test', 'test-group-2@test-tenant@choirconcierge.test'])
+            ->from('sender@example.com')
+            ->subject('Just a test');
+
+        // Act
+        $groups_found = $message->getMatchingGroups()->flatten(1);
+
+        // Assert
+        self::assertCount(2, $groups_found);
     }
 
-	public function recipientsToRejectProvider(): array
-	{
-		return [
-			'Reject FROM matches CC' => [
-                'groups' => [
-                    ['name' => 'Music Team', 'tenant' => 'tenant1'],
-                ],
-				'input' => [
-					'to'     => 'somebody@example.com',
-					'cc'     => 'music-team@tenant1.choirconcierge.test',
-					'bcc'    => 'nobody@example.com',
-					'from'   => 'music-team@tenant1.choirconcierge.test',
-				],
-				false,
-			],
-//			@todo reject for non-existent tenant
-//			'Reject TO has matching slug but wrong domain' => [
-//				'input' => [
-//					'to'     => 'music-team@wrong.com',
-//					'cc'     => 'somebody@example.com',
-//					'bcc'    => 'nobody@example.com',
-//					'from'   => 'nothing@example.com',
-//				],
-//				false,
-//			],
-		];
-	}
+    /** @test */
+    public function getMatchingGroups_matches_groups_in_any_recipient_field(): void
+    {
+        // Arrange
+        $tenant = Tenant::create(
+            'test-tenant',
+            'Test Tenant',
+            'Australia/Perth',
+        );
+
+        $tenant->run(function () {
+            UserGroup::create([
+                'title'     => 'Test Group 1',
+                'slug'      => 'test-group-1',
+                'list_type' => 'chat',
+            ]);
+            UserGroup::create([
+                'title'     => 'Test Group 2',
+                'slug'      => 'test-group-2',
+                'list_type' => 'chat',
+            ]);
+            UserGroup::create([
+                'title'     => 'Test Group 3',
+                'slug'      => 'test-group-3',
+                'list_type' => 'chat',
+            ]);
+        });
+
+        $message = (new IncomingMessage())
+            ->to('test-group-1@test-tenant@choirconcierge.test')
+            ->cc('test-group-2@test-tenant@choirconcierge.test')
+            ->bcc('test-group-3@test-tenant@choirconcierge.test')
+            ->from('sender@example.com')
+            ->subject('Just a test');
+
+        // Act
+        $groups_found = $message->getMatchingGroups()->flatten(1);
+
+        // Assert
+        self::assertCount(3, $groups_found);
+    }
+
+    /** @test */
+    public function getMatchingGroups_matches_groups_in_any_position_in_the_recipient_field(): void
+    {
+        // Arrange
+        $tenant = Tenant::create(
+            'test-tenant',
+            'Test Tenant',
+            'Australia/Perth',
+        );
+
+        $tenant->run(function () {
+            UserGroup::create([
+                'title'     => 'Test Group 1',
+                'slug'      => 'test-group-1',
+                'list_type' => 'chat',
+            ]);
+            UserGroup::create([
+                'title'     => 'Test Group 2',
+                'slug'      => 'test-group-2',
+                'list_type' => 'chat',
+            ]);
+            UserGroup::create([
+                'title'     => 'Test Group 3',
+                'slug'      => 'test-group-3',
+                'list_type' => 'chat',
+            ]);
+        });
+
+        $message = (new IncomingMessage())
+            ->to(['dummy@example.com', 'test-group-1@test-tenant@choirconcierge.test'])
+            ->cc(['dummy@example.com', 'test-group-2@test-tenant@choirconcierge.test'])
+            ->bcc(['dummy@example.com', 'test-group-3@test-tenant@choirconcierge.test'])
+            ->from('sender@example.com')
+            ->subject('Just a test');
+
+        // Act
+        $groups_found = $message->getMatchingGroups()->flatten(1);
+
+        // Assert
+        self::assertCount(3, $groups_found);
+    }
+
+    /** @test */
+    public function getMatchingGroups_matches_groups_from_any_tenant(): void
+    {
+        // Arrange
+        $tenant_1 = Tenant::create(
+            'test-tenant-1',
+            'Test Tenant 1',
+            'Australia/Perth',
+        );
+        $tenant_2 = Tenant::create(
+            'test-tenant-2',
+            'Test Tenant 2',
+            'Australia/Perth',
+        );
+
+        $tenant_1->run(function () {
+            UserGroup::create([
+                'title'     => 'Test Group 1',
+                'slug'      => 'test-group-1',
+                'list_type' => 'chat',
+            ]);
+        });
+        $tenant_2->run(function () {
+            UserGroup::create([
+                'title'     => 'Test Group 2',
+                'slug'      => 'test-group-2',
+                'list_type' => 'chat',
+            ]);
+        });
+
+        $message = (new IncomingMessage())
+            ->to(['test-group-1@test-tenant-1@choirconcierge.test', 'test-group-2@test-tenant-2@choirconcierge.test'])
+            ->from('sender@example.com')
+            ->subject('Just a test');
+
+        // Act
+        $groups_found = $message->getMatchingGroups()->flatten(1);
+
+        // Assert
+        self::assertCount(2, $groups_found);
+    }
+
+    /** @test */
+    public function getMatchingGroups_rejects_groups_that_are_in_both_the_cc_and_from(): void
+    {
+        // Arrange
+        $tenant = Tenant::create(
+            'test-tenant',
+            'Test Tenant',
+            'Australia/Perth',
+        );
+
+        $tenant->run(function () {
+            UserGroup::create([
+                'title'     => 'Test Group',
+                'slug'      => 'test-group',
+                'list_type' => 'chat',
+            ]);
+        });
+
+        $message = (new IncomingMessage())
+            ->to('dummy@example.com')
+            ->cc('test-group@test-tenant.choirconcierge.test')
+            ->from('test-group@test-tenant.choirconcierge.test')
+            ->subject('Just a test');
+
+        // Act
+        $groups_found = $message->getMatchingGroups()->flatten(1);
+
+        // Assert
+        self::assertCount(0, $groups_found);
+    }
+
 }
