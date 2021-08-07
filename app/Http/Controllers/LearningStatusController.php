949e5fe1eee94b8d4fd0102ce76881558f2ff1f2
Hayden Bech <haydenbech@gmail.com>
2021-08-07 20:15:58 +0800

    Create missing learning records when viewing report

--- app/Http/Controllers/LearningStatusController.php
+++ app/Http/Controllers/LearningStatusController.php
@@ -2,8 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\LearningStatus;
-use App\Models\Singer;
 use App\Models\Song;
 use Illuminate\Contracts\View\View;
 
@@ -13,21 +11,13 @@ class LearningStatusController extends Controller
     {
         $this->authorize('update', $song);
 
-        $song->load('singers');
+        $song->createMissingLearningRecords();
 
-        $singers_with = $song->singers()->with(['user', 'voice_part'])->get();
-        $singers_without = Singer::with(['user', 'voice_part'])->whereDoesntHave('songs', function($query) use ($song) {
-            $query->where('song_id', '=', $song->id);
-        })
-        ->get()
-        ->each(function(Singer $singer) {
-            $singer->learning = LearningStatus::getNullLearningStatus();
-        });
-        $singers = $singers_with->merge($singers_without);
+        $song->load('singers');
 
         return view('songs.learning.index', [
             'song' => $song,
-            'singers' => $singers,
+            'singers' => $song->singers()->with(['user', 'voice_part'])->get(),
         ]);
     }
 }
