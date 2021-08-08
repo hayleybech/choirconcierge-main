30921ebc698fd2eaed64a7a850aa67028090a6a9
Hayden Bech <haydenbech@gmail.com>
2021-08-08 13:37:10 +0800

    Create all missing learning records when updating mine

--- app/Http/Controllers/UpdateMyLearningStatusController.php
+++ app/Http/Controllers/UpdateMyLearningStatusController.php
@@ -10,11 +10,9 @@ class UpdateMyLearningStatusController extends Controller
 {
     public function __invoke(Song $song, Request $request): RedirectResponse
     {
-        if($song->my_learning->status === 'not-started'){
-            $song->singers()->attach(auth()->user()->singer->id, ['status' => 'assessment-ready']);
-        } else {
-            $song->singers()->updateExistingPivot(auth()->user()->singer->id, ['status' => 'assessment-ready']);
-        }
+        $song->createMissingLearningRecords();
+        
+        $song->singers()->updateExistingPivot(auth()->user()->singer->id, ['status' => 'assessment-ready']);
 
         return redirect()->route('songs.show', $song);
     }
