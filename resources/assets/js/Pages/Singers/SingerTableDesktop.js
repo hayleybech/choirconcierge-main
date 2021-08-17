15e045efb61f2b6c3cb65e20ccba2afc5f5f655e
Hayden Bech <haydenbech@gmail.com>
2021-08-17 19:28:52 +0800

    Create SingerTableDesktop component

---
+++ resources/assets/js/Pages/Singers/SingerTableDesktop.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import {Link} from "@inertiajs/inertia-react";
+import VoicePartTag from "../../components/VoicePartTag";
+import SingerCategoryTag from "../../components/SingerCategoryTag";
+
+const SingerTableDesktop = ({singers}) => (
+    <div className="-my-2 overflow-x-auto">
+        <div className="py-2 align-middle inline-block min-w-full">
+            <div className="shadow overflow-hidden border-b border-gray-200">
+                <table className="min-w-full divide-y divide-gray-200">
+                    <thead className="bg-gray-50">
+                    <tr>
+                        <th
+                            scope="col"
+                            className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                        >
+                            Name
+                        </th>
+                        <th
+                            scope="col"
+                            className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                        >
+                            Voice Part
+                        </th>
+                        <th
+                            scope="col"
+                            className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                        >
+                            Status
+                        </th>
+                        <th
+                            scope="col"
+                            className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                        >
+                            Email
+                        </th>
+                        <th scope="col" className="relative px-6 py-3">
+                            <span className="sr-only">Edit</span>
+                        </th>
+                    </tr>
+                    </thead>
+                    <tbody className="bg-white divide-y divide-gray-200">
+                    {singers.map((singer) => (
+                        <tr key={singer.id}>
+                            <td className="px-6 py-4 whitespace-nowrap">
+                                <div className="flex items-center">
+                                    <div className="flex-shrink-0 h-10 w-10">
+                                        <img className="h-10 w-10 rounded-md" src={singer.user.avatar_url} alt="" />
+                                    </div>
+                                    <div className="ml-4">
+                                        <Link href={route('singers.show', singer.id)} className="text-sm font-medium text-purple-800">{singer.user.name}</Link>
+                                        <div className="text-sm text-gray-500"><i className="fas fa-fw fa-phone mr-1.5" /> {singer.user.phone ?? 'No phone'}</div>
+                                    </div>
+                                </div>
+                            </td>
+                            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                {singer.voice_part && <VoicePartTag title={singer.voice_part.title} colour={singer.voice_part.colour} />}
+                            </td>
+                            <td className="px-6 py-4 whitespace-nowrap">
+                                <SingerCategoryTag name={singer.category.name} colour={singer.category.colour} withLabel />
+                            </td>
+                            <td className="px-6 py-4 whitespace-nowrap">
+                                <div className="text-sm text-gray-500">
+                                    <i className="fas fa-fw fa-envelope mr-1.5" />
+                                    <span>{singer.user.email}</span>
+                                </div>
+                            </td>
+                            <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                                <a href="#" className="text-indigo-600 hover:text-indigo-900">
+                                    Edit
+                                </a>
+                            </td>
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+);
+
+export default SingerTableDesktop;
