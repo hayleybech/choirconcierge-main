30532bb44d9275903ce1dff8890286db63d10d7e
Hayden Bech <haydenbech@gmail.com>
2021-08-20 16:48:54 +0800

    Install sentry react

--- resources/assets/js/app-rebuild.js
+++ resources/assets/js/app-rebuild.js
@@ -2,9 +2,21 @@ import React from 'react'
 import { render } from 'react-dom'
 import { createInertiaApp } from '@inertiajs/inertia-react'
 import { InertiaProgress } from '@inertiajs/progress'
+import * as Sentry from '@sentry/react';
+import {Integrations as TracingIntegrations} from "@sentry/tracing";
 
-// @todo install sentry react
-const VERSION = 'choir-concierge@2021-08-10a';
+const VERSION = 'choir-concierge@2021-08-18a';
+
+Sentry.init({
+	dsn: process.env.MIX_SENTRY_DSN,
+	logErrors: true,
+	integrations: [new TracingIntegrations.BrowserTracing()],
+	tracesSampleRate: process.env.MIX_SENTRY_TRACES_SAMPLE_RATE,
+	tracingOptions: {
+		trackComponents: true,
+	},
+	release: process.env.MIX_APP_ENV === 'production' ? VERSION : VERSION + ':dev',
+})
 
 createInertiaApp({
 	resolve: name => require(`./Pages/${name}`),
