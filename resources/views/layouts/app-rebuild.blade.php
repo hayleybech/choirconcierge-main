2bc86d3eae2bee944d4bc114e8a1c82baad978f0
Hayden Bech <haydenbech@gmail.com>
2021-08-20 16:51:59 +0800

    Re-enable hotjar for react

--- resources/views/layouts/app-rebuild.blade.php
+++ resources/views/layouts/app-rebuild.blade.php
@@ -32,13 +32,13 @@
     <!-- Scripts -->
     <script src="{{ mix('/js/app-rebuild.js') }}"></script>
 
-{{--	<script>--}}
-{{--	  var HW_config = {--}}
-{{--		selector: ".headway-badge", // CSS selector where to inject the badge--}}
-{{--		account:  "7L6Rky"--}}
-{{--	  }--}}
-{{--	</script>--}}
-{{--	<script async src="//cdn.headwayapp.co/widget.js"></script>--}}
+	<script>
+	  var HW_config = {
+		selector: ".headway-badge", // CSS selector where to inject the badge
+		account:  "7L6Rky"
+	  }
+	</script>
+	<script async src="//cdn.headwayapp.co/widget.js"></script>
 
 	@stack('scripts-footer-bottom')
 
