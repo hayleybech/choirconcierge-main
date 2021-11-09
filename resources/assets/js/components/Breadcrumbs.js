7e319cb045aaad115185b035962dfaf8d564b8de
Hayden Bech <haydenbech@gmail.com>
2021-11-09 16:50:10 +0800

    Fix crumb back button

--- resources/assets/js/components/Breadcrumbs.js
+++ resources/assets/js/components/Breadcrumbs.js
@@ -2,31 +2,38 @@ import React from 'react';
 import {ChevronLeftIcon, ChevronRightIcon} from "@heroicons/react/solid";
 import {Link} from "@inertiajs/inertia-react";
 
-const Breadcrumbs = ({breadcrumbs}) => (
-    <div>
-        <nav className="sm:hidden" aria-label="Back">
-            <Link href="#" className="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700">
-                <ChevronLeftIcon className="flex-shrink-0 -ml-1 mr-1 h-5 w-5 text-gray-400" aria-hidden="true" />
-                Back
-            </Link>
-        </nav>
-        <nav className="hidden sm:flex" aria-label="Breadcrumb">
-            <ol className="flex items-center space-x-4">
-                {breadcrumbs.map(({name, url}, index) => (
-                    <li key={index}>
-                        <div className="flex items-center">
-                            {index > 0 && (
-                                <ChevronRightIcon className="flex-shrink-0 h-5 w-5 text-gray-400 mr-4" aria-hidden="true" />
-                            )}
-                            <Link href={url} className="text-sm font-medium text-gray-500 hover:text-gray-700">
-                                {name}
-                            </Link>
-                        </div>
-                    </li>
-                ))}
-            </ol>
-        </nav>
-    </div>
-);
+const Breadcrumbs = ({ breadcrumbs }) => {
+    const parentCrumb = breadcrumbs[breadcrumbs.length - 2] ?? breadcrumbs[0];
+    
+    return (
+        <div>
+            <nav className="sm:hidden" aria-label="Back">
+                <Link
+                    href={parentCrumb.url}
+                    className="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700"
+                >
+                    <ChevronLeftIcon className="flex-shrink-0 -ml-1 mr-1 h-5 w-5 text-gray-400" aria-hidden="true" />
+                    {parentCrumb.name}
+                </Link>
+            </nav>
+            <nav className="hidden sm:flex" aria-label="Breadcrumb">
+                <ol className="flex items-center space-x-4">
+                    {breadcrumbs.map(({name, url}, index) => (
+                        <li key={index}>
+                            <div className="flex items-center">
+                                {index > 0 && (
+                                    <ChevronRightIcon className="flex-shrink-0 h-5 w-5 text-gray-400 mr-4" aria-hidden="true" />
+                                )}
+                                <Link href={url} className="text-sm font-medium text-gray-500 hover:text-gray-700">
+                                    {name}
+                                </Link>
+                            </div>
+                        </li>
+                    ))}
+                </ol>
+            </nav>
+        </div>
+    );
+}
 
 export default Breadcrumbs;
