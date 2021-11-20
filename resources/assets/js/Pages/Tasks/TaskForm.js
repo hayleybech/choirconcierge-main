2ae3e66fd6495b9a851e64a73fd825edd7dab8c8
Hayden Bech <haydenbech@gmail.com>
2021-11-20 12:05:17 +0800

    Make task create form wider

--- resources/assets/js/Pages/Tasks/TaskForm.js
+++ resources/assets/js/Pages/Tasks/TaskForm.js
@@ -22,7 +22,7 @@ const TaskForm = ({ roles }) => {
     }
 
     return (
-        <div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+        <div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8 py-8 w-full">
             <Form onSubmit={submit}>
 
                 <FormSection title="Task Details">
