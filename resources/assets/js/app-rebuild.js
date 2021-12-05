01b1ebe6ead0d83142f7088fd3c85ffc53e166d2
Hayden Bech <haydenbech@gmail.com>
2021-12-05 21:54:45 +0800

    Bump version

--- resources/assets/js/app-rebuild.js
+++ resources/assets/js/app-rebuild.js
@@ -5,7 +5,7 @@ import { InertiaProgress } from '@inertiajs/progress'
 import * as Sentry from '@sentry/react';
 import {Integrations as TracingIntegrations} from "@sentry/tracing";
 
-const VERSION = 'choir-concierge@2021-12-03a';
+const VERSION = 'choir-concierge@2021-12-05a';
 
 Sentry.init({
 	dsn: process.env.MIX_SENTRY_DSN,
