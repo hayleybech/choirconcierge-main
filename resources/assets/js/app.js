755ca0daae61c10d4980faa765a0c326d1d1b9bc
Hayden Bech <haydenbech@gmail.com>
2021-12-23 13:05:06 +0800

    Bump version

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -3,7 +3,7 @@ import ReadMore from 'vue-read-more';
 import * as Sentry from '@sentry/vue';
 import { Integrations as TracingIntegrations } from '@sentry/tracing';
 
-const VERSION = 'choir-concierge@2021-12-06a';
+const VERSION = 'choir-concierge@2021-12-23a';
 
 console.log('test');
 require('./bootstrap');
