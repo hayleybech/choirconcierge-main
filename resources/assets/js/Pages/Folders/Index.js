68938ad13f87b662837794dfa4547234b74186c4
Hayden Bech <haydenbech@gmail.com>
2022-01-04 11:56:10 +0800

    Add delete folder and delete document buttons

--- resources/assets/js/Pages/Folders/Index.js
+++ resources/assets/js/Pages/Folders/Index.js
@@ -1,13 +1,17 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Layout from "../../Layouts/Layout";
 import PageHeader from "../../components/PageHeader";
 import AppHead from "../../components/AppHead";
 import FolderTableDesktop from "./FolderTableDesktop";
 import FolderTableMobile from "./FolderTableMobile";
 import {usePage} from "@inertiajs/inertia-react";
+import DeleteDialog from "../../components/DeleteDialog";
 
 const Index = ({ folders }) => {
     const { can } = usePage().props;
+
+    const [deletingFolder, setDeletingFolder] = useState(null);
+    const [deletingDocument, setDeletingDocument] = useState(null);
 
     return (
         <>
@@ -25,12 +29,29 @@ const Index = ({ folders }) => {
             />
 
             <div className="hidden lg:flex flex-col">
-                <FolderTableDesktop folders={folders} />
+                <FolderTableDesktop folders={folders} setDeletingFolder={setDeletingFolder} setDeletingDocument={setDeletingDocument} />
             </div>
 
             <div className="bg-white shadow block lg:hidden">
                 <FolderTableMobile folders={folders} />
             </div>
+
+            <DeleteDialog title="Delete Folder" url={deletingFolder ? route('folders.destroy', deletingFolder) : '#'} isOpen={!!deletingFolder} setIsOpen={setDeletingFolder}>
+                Are you sure you want to delete this folder?
+                All of its documents will be permanently removed from our servers forever.
+                This action cannot be undone.
+            </DeleteDialog>
+
+            <DeleteDialog
+                title="Delete Document"
+                url={deletingDocument ? route('folders.documents.destroy', [deletingDocument.folder_id, deletingDocument]) : '#'}
+                isOpen={!!deletingDocument}
+                setIsOpen={setDeletingDocument}
+            >
+                Are you sure you want to delete this document?
+                It will be permanently removed from our servers forever.
+                This action cannot be undone.
+            </DeleteDialog>
         </>
     );
 }
