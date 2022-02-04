54b3238afd10072fd7a047331eaa4e68fcb29a7d
Hayden Bech <haydenbech@gmail.com>
2022-02-04 18:11:09 +0800

    Add singer pages to account breadcrumbs

--- resources/assets/js/Pages/Account/Edit.js
+++ resources/assets/js/Pages/Account/Edit.js
@@ -3,22 +3,29 @@ import Layout from "../../Layouts/Layout";
 import PageHeader from "../../components/PageHeader";
 import AppHead from "../../components/AppHead";
 import AccountForm from "./AccountForm";
+import {usePage} from "@inertiajs/inertia-react";
 
-const Edit = ({ }) => (
-    <>
-        <AppHead title="Edit Profile" />
-        <PageHeader
-            title="Edit Profile"
-            icon="user-edit"
-            breadcrumbs={[
-                { name: 'Dashboard', url: route('dash')},
-                { name: 'Edit Profile', url: route('accounts.edit')},
-            ]}
-        />
+const Edit = ({ }) => {
+    const { user: authUser } = usePage().props;
 
-        <AccountForm />
-    </>
-);
+    return (
+        <>
+            <AppHead title="Edit Profile" />
+            <PageHeader
+                title="Edit Profile"
+                icon="user-edit"
+                breadcrumbs={[
+                    { name: 'Dashboard', url: route('dash')},
+                    { name: 'Singers', url: route('singers.index')},
+                    { name: authUser.name, url: route('singers.show', authUser) },
+                    { name: 'Edit Profile', url: route('accounts.edit')},
+                ]}
+            />
+
+            <AccountForm />
+        </>
+    );
+}
 
 Edit.layout = page => <Layout children={page} />
 
