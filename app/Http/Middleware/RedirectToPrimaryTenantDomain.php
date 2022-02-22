3bca25c60910623eac36023aa2055ce5e49ef0a9
Hayden Bech <haydenbech@gmail.com>
2022-02-22 17:54:00 +0800

    Fix type error

--- app/Http/Middleware/RedirectToPrimaryTenantDomain.php
+++ app/Http/Middleware/RedirectToPrimaryTenantDomain.php
@@ -3,16 +3,14 @@
 namespace App\Http\Middleware;
 
 use Closure;
-use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 
 class RedirectToPrimaryTenantDomain
 {
     /**
      * Handle an incoming request.
      */
-    public function handle(Request $request, Closure $next): Response|RedirectResponse
+    public function handle(Request $request, Closure $next)
     {
         if(tenancy()->initialized && $request->getHost() !== tenant('host')) {
             return redirect('//'.tenant('host'));
