a9fcb68976c6d0594a518c786ef7f4a149fd4c9f
Hayden Bech <haydenbech@gmail.com>
2022-03-06 21:09:36 +0800

    Make prose headings less heavy

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,7 @@ module.exports = {
     './resources/assets/js/*.js',
   ],
   theme: {
-    fontFamily: {
-      'sans': 'Lato, Arial, Helvetica, sans-serif'
-    },
+    fontFamily: { 'sans': 'Lato, Arial, Helvetica, sans-serif' },
     extend: {
       colors: {
         'brand-purple-dark': '#200142',
@@ -16,15 +14,14 @@ module.exports = {
         'brand-off-white': '#f2f1f3',
         'brand-blue': '#21a7eb',
       },
-      width: {
-        '250px': '250px',
-      },
+      width: { '250px': '250px' },
       typography: (theme) => ({
         DEFAULT: {
           css: {
-            a: {
-              color: theme('colors.purple.500')
-            }
+            a: { color: theme('colors.purple.500') },
+            h1: { fontWeight: 300 },
+            h2: { fontWeight: 300 },
+            h3: { fontWeight: 300 },
           }
         }
       }),
