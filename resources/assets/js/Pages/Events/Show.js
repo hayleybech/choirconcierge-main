97c0076a800f6c594c2c7b394f772593b3cc991d
Hayden Bech <haydenbech@gmail.com>
2022-03-06 20:00:12 +0800

    Add rich text to notification templates

--- resources/assets/js/Pages/Events/Show.js
+++ resources/assets/js/Pages/Events/Show.js
@@ -14,6 +14,7 @@ import {usePage} from "@inertiajs/inertia-react";
 import Icon from "../../components/Icon";
 import EditRepeatingEventDialog from "../../components/Event/EditRepeatingEventDialog";
 import DeleteDialog from "../../components/DeleteDialog";
+import Prose from "../../components/Prose";
 
 const Show = ({ event, rsvpCount, voicePartsRsvpCount, attendanceCount, voicePartsAttendanceCount, addToCalendarLinks }) => {
     const [deleteDialogIsOpen, setDeleteDialogIsOpen] = useState(false);
@@ -73,7 +74,7 @@ const Show = ({ event, rsvpCount, voicePartsRsvpCount, attendanceCount, voicePar
                             <SectionTitle>Event Description</SectionTitle>
                         </SectionHeader>
 
-                        <div className="mb-8 prose" dangerouslySetInnerHTML={{ __html: event.description }} />
+                        <Prose content={event.description} className="mb-8" />
 
                         <p className="text-sm text-gray-500 my-2">Choir's Timezone: {pageProps.tenant.timezone_label}</p>
                     </div>
--- resources/assets/js/components/inputs/RichTextInput.js
+++ resources/assets/js/components/inputs/RichTextInput.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactQuill from "react-quill";
 import 'react-quill/dist/quill.snow.css';
+import {nl2br} from "../../util";
 
 const RichTextInput = ({ value, updateFn }) => (
-    <ReactQuill value={value} onChange={updateFn} theme="snow" />
+    <ReactQuill value={nl2br(value)} onChange={updateFn} theme="snow" />
 );
 
 export default RichTextInput;
