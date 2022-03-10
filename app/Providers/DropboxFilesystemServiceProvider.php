e51833e4660dc7b52e966262e19efa4eeb08f113
Hayden Bech <haydenbech@gmail.com>
2022-03-10 19:00:44 +0800

    Update custom filesystem

--- app/Providers/DropboxFilesystemServiceProvider.php
+++ app/Providers/DropboxFilesystemServiceProvider.php
@@ -2,20 +2,26 @@
 
 namespace App\Providers;
 
+use Illuminate\Filesystem\FilesystemAdapter;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\ServiceProvider;
 use League\Flysystem\Filesystem;
 use Spatie\Dropbox\Client as DropboxClient;
 use Spatie\FlysystemDropbox\DropboxAdapter;
-use Storage;
 
 class DropboxFilesystemServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
         Storage::extend('dropbox', function ($app, $config) {
-            $client = new DropboxClient($config['accessToken']);
+            $adapter = new DropboxAdapter(new DropboxClient($config['accessToken']));
 
-            return new Filesystem(new DropboxAdapter($client));
+            return new FilesystemAdapter(
+                new Filesystem($adapter, $config),
+                $adapter,
+                $config
+
+            );
         });
     }
 
