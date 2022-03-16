496568dada2c4e80904031748b0c3974ba0f89ea
Hayden Bech <haydenbech@gmail.com>
2022-03-16 17:43:12 +0800

    Add new imap config

--- tests/Unit/Jobs/ProcessGroupMailboxTest.php
+++ tests/Unit/Jobs/ProcessGroupMailboxTest.php
@@ -5,11 +5,10 @@ namespace Tests\Unit\Jobs;
 use App\Jobs\ProcessGroupMailbox;
 use App\Mail\IncomingMailbox;
 use App\Mail\IncomingMessage;
-use App\Mail\WebklexImapMessageMailableAdapter;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Mockery\MockInterface;
 use Tests\TestCase;
-use Webklex\IMAP\Message;
+use Webklex\PHPIMAP\Message;
 
 /**
  * @see \App\Jobs\ProcessGroupMailbox
