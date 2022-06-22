1b3ae489a8f20885133bdb4a87fba3f554598081
Hayden Bech <haydenbech@gmail.com>
2022-06-22 19:55:55 +0800

    Bump version

--- config/sentry.php
+++ config/sentry.php
@@ -1,6 +1,6 @@
 <?php
 
-$VERSION = 'choir-concierge@2022-06-21a';
+$VERSION = 'choir-concierge@2022-06-22a';
 
 return [
 
--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -5,7 +5,7 @@ import { InertiaProgress } from '@inertiajs/progress'
 import * as Sentry from '@sentry/react';
 import {Integrations as TracingIntegrations} from "@sentry/tracing";
 
-const VERSION = 'choir-concierge@2022-06-21a';
+const VERSION = 'choir-concierge@2022-06-22a';
 
 Sentry.init({
 	dsn: process.env.MIX_SENTRY_DSN,
