e61024370cd94137c3d4819407005f7ea7c9bd81
Hayden Bech <haydenbech@gmail.com>
2022-06-22 19:54:10 +0800

    Conditionally render large device-specific components

--- resources/assets/js/components/SidebarMobile.js
+++ resources/assets/js/components/SidebarMobile.js
@@ -7,7 +7,7 @@ import Icon from "./Icon";
 
 const SidebarMobile = ({ navigation, open, setOpen, switchChoirButton }) => (
     <Transition.Root show={open} as={Fragment}>
-        <Dialog as="div" className="fixed inset-0 flex z-40 xl:hidden" onClose={setOpen}>
+        <Dialog as="div" className="fixed inset-0 flex z-40" onClose={setOpen}>
             <Transition.Child
                 as={Fragment}
                 enter="transition-opacity ease-linear duration-300"
