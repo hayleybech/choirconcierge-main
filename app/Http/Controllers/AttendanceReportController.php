b67eafdafc8608511946ffbec28e384cf5156d0e
Hayden Bech <haydenbech@gmail.com>
2022-07-12 19:36:54 +0800

    Clean up attendance controllers

--- app/Http/Controllers/AttendanceReportController.php
+++ app/Http/Controllers/AttendanceReportController.php
@@ -6,13 +6,13 @@ use App\Models\Attendance;
 use App\Models\Event;
 use App\Models\Singer;
 use App\Models\VoicePart;
-use Illuminate\Contracts\View\View;
+use Illuminate\Database\Eloquent\Collection;
 use Inertia\Inertia;
 use Inertia\Response;
 
 class AttendanceReportController extends Controller
 {
-    public function __invoke(): View|Response
+    public function __invoke(): Response
     {
         $this->authorize('viewAny', Attendance::class);
 
@@ -20,29 +20,12 @@ class AttendanceReportController extends Controller
             ->orderBy('start_date')
             ->get();
 
-        $singers = Singer::with(['user', 'attendances'])
-            ->get()
-            ->each
-            ->append('user_avatar_thumb_url')
-            ->each(function ($singer) use ($all_events) {
-                $singer->timesPresent = $singer->attendances->where('response', 'present')->count();
-                $singer->percentPresent = floor($singer->timesPresent / $all_events->count() * 100);
-            });
+        $singers = $this->getSingers($all_events);
 
         $all_events->each(function ($event) use ($singers) {
             $event->singersPresent = $event->singers_attendance('present')->active()->get()->count();
             $event->percentPresent = floor($event->singersPresent / $singers->count() * 100);
         });
-
-        $voice_parts = VoicePart::all()
-            ->push(VoicePart::getNullVoicePart())
-            ->map(function ($part) use ($singers) {
-                $part->singers = $singers
-                    ->filter(fn ($singer) => $singer->voice_part_id === $part->id)
-                    ->values();
-
-                return $part;
-            });
 
         $avg_singers_per_event = round(
             $all_events->reduce(static function ($carry, $event) {
@@ -51,26 +34,52 @@ class AttendanceReportController extends Controller
             2,
         );
 
-        $avg_events_per_singer = round(
-            Singer::active()
-                ->with(['attendances'])
-                ->get()
-                ->reduce(static function ($carry, $singer) {
-                    return $carry +
-                        $singer
-                            ->attendances()
-                            ->where('response', 'present')
-                            ->count();
-                }, 0) / Singer::all()->count(),
-            2,
-        );
-
         return Inertia::render('Events/AttendanceReport', [
-            'voiceParts' => $voice_parts->values(),
+            'voiceParts' => $this->getVoiceParts($singers)->values(),
             'events' => $all_events->where('start_date', '<', now())->values(),
             'numSingers' => $singers->count(),
             'avgSingersPerEvent' => $avg_singers_per_event,
-            'avgEventsPerSinger' => $avg_events_per_singer,
+            'avgEventsPerSinger' => $this->getAverageEventsPerSinger(),
         ]);
+    }
+
+    private function getAverageEventsPerSinger(): float
+    {
+        return round(
+            Singer::active()
+                ->with(['attendances'])
+                ->get()
+                ->reduce(fn ($carry, $singer) =>
+                    $carry + $singer
+                        ->attendances()
+                        ->where('response', 'present')
+                        ->count()
+                , 0) / Singer::all()->count(),
+            2,
+        );
+    }
+
+    private function getVoiceParts($singers): \Illuminate\Support\Collection|Collection
+    {
+        return VoicePart::all()
+            ->push(VoicePart::getNullVoicePart())
+            ->map(function ($part) use ($singers) {
+                $part->singers = $singers
+                    ->filter(fn($singer) => $singer->voice_part_id === $part->id)
+                    ->values();
+
+                return $part;
+            });
+    }
+
+    private function getSingers(Collection $events)
+    {
+        return Singer::with(['user', 'attendances'])
+            ->get()
+            ->append('user_avatar_thumb_url')
+            ->each(function ($singer) use ($events) {
+                $singer->timesPresent = $singer->attendances->where('response', 'present')->count();
+                $singer->percentPresent = floor($singer->timesPresent / $events->count() * 100);
+            });
     }
 }
