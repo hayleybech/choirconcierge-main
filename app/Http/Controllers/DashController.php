9f24c16d6b6ce1ddd16b6823177cb4b750d6a20b
Hayden Bech <haydenbech@gmail.com>
2022-07-12 17:52:02 +0800

    Clean up dashboard query logic

--- app/Http/Controllers/DashController.php
+++ app/Http/Controllers/DashController.php
@@ -9,82 +9,73 @@ use App\Models\Song;
 use App\Models\User;
 use Illuminate\Contracts\View\View;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Validation\Rules\In;
 use Inertia\Inertia;
+use Inertia\Response;
 
 class DashController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
     public function __construct()
     {
         $this->middleware('auth');
     }
 
-    /**
-     * Show the application dashboard.
-     */
-    public function index(): \Inertia\Response|View
+    public function index(): Response
     {
-        $birthdays = User::query()
-            ->whereHas('singers', fn ($query) => $query->active())
-            ->birthdays()
-            ->get()
-            ->each->append('birthday')
-            ->sort(static function (User $user1, User $user2): int {
-                // Sort by birthday
+        return Inertia::render('Dash/Show', [
+            'events' => $this->getEvents()->values(),
+            'songs' => $this->getSongs()->values(),
+            'birthdays' => $this->getBirthdays()->values(),
+            'emptyDobs' => $this->getEmptyDobs(),
+            'memberversaries' => $this->getMemberversaries()->values(),
+	        'feeStatus' => auth()->user()->singer->fee_status,
+        ]);
+    }
 
-                if ($user1->birthday->equalTo($user2->birthday)) {
-                    return 0;
-                }
-
-                return $user1->birthday < $user2->birthday ? -1 : 1;
-            });
-
-        $memberversaries = Singer::query()
+    private function getMemberversaries()
+    {
+        return Singer::query()
             ->with('user')
             ->active()
             ->memberversaries()
             ->get()
             ->append('memberversary')
-            ->sort(static function (Singer $singer1, Singer $singer2): int {
-                // Sort by joined date
-                if ($singer1->joined_at->equalTo($singer2->joined_at)) {
-                    return 0;
-                }
+            ->sortBy('joined_at');
+    }
 
-                return $singer1->joined_at < $singer2->joined_at ? -1 : 1;
-            });
+    private function getEvents(): Collection
+    {
+        return Event::query()
+            ->with(['my_rsvp'])
+            ->whereBetween('call_time', [today(), today()->addMonth()])
+            ->orderBy('call_time')
+            ->get();
+    }
 
-        $empty_dobs = Singer::query()
+    private function getSongs()
+    {
+        return Song::whereHas('status', fn (Builder $query) => $query->where('title', 'Learning'))
+            ->orderBy('title')
+            ->get()
+            ->append('my_learning');
+    }
+
+    private function getEmptyDobs()
+    {
+        return Singer::query()
             ->with('user')
             ->emptyDobs()
             ->count();
+    }
 
-        $events = Event::query()
-            ->with(['my_rsvp'])
-            ->where('call_time', '>', today())
-            ->where('call_time', '<', today()->addMonth())
-            ->orderBy('call_time')
-            ->get();
-
-        $songs = Song::whereHas('status', static function (Builder $query) {
-            return $query->where('title', 'Learning');
-        })
-            ->orderBy('title')
+    private function getBirthdays()
+    {
+        return User::query()
+            ->whereHas('singers', fn($query) => $query->active())
+            ->birthdays()
             ->get()
-            ->each->append('my_learning');
-
-        return Inertia::render('Dash/Show', [
-            'events' => $events->values(),
-            'songs' => $songs->values(),
-            'birthdays' => $birthdays->values(),
-            'emptyDobs' => $empty_dobs,
-            'memberversaries' => $memberversaries->values(),
-	        'feeStatus' => auth()->user()->singer->fee_status,
-        ]);
+            ->append('birthday')
+            ->sortBy('birthday');
     }
 }
