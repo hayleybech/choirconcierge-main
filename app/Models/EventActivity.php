bc488e5c59b0e8e57d330153dc86a55481ea8b1b
Hayden Bech <haydenbech@gmail.com>
2022-07-20 14:20:27 +0800

    Add model comment

--- app/Models/EventActivity.php
+++ app/Models/EventActivity.php
@@ -5,6 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Columns
+ * @property int $id
+ * @property int $event_id
+ * @property string $description
+ * @property int $duration
+ */
+
 class EventActivity extends Model
 {
     use HasFactory;
