2a5a003af62263d7650ef0895b603c6a764991c0
Hayden Bech <haydenbech@gmail.com>
2022-07-30 09:10:05 +0800

    Fix voice placement form width

--- resources/assets/js/Pages/Singers/Placements/PlacementForm.js
+++ resources/assets/js/Pages/Singers/Placements/PlacementForm.js
@@ -29,7 +29,7 @@ const PlacementForm = ({ singer, placement, voice_parts }) => {
     }
 
     return (
-        <div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+        <div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8 py-8 w-full">
             <Form onSubmit={submit}>
 
                 <FormSection title="Placement Details">
