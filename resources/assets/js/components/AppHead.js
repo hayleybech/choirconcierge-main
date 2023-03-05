91a7d2c0649ede0a2cbb2dc28a27ae5da5cfcb21
Hayden Bech <haydenbech@gmail.com>
2023-03-05 21:24:05 +0800

    Create PWA

--- resources/assets/js/components/AppHead.js
+++ resources/assets/js/components/AppHead.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import { Head } from '@inertiajs/inertia-react'
 
-const AppHead = ({ title, children }) => {
-    return (
-        <Head>
-            <title>{title ? `${title} | Choir Concierge` : 'Choir Concierge'}</title>
-            {children}
-        </Head>
-    )
-}
+const AppHead = ({ title, children }) => (
+    <Head>
+        <title>{title ? `${title} | Choir Concierge` : 'Choir Concierge'}</title>
+        {children}
+    </Head>
+);
 
 export default AppHead;
