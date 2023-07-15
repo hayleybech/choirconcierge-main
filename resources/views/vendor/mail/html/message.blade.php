03a500dca5e139b3eea543d0a162d4a930cfd57a
Hayden Bech <haydenbech@gmail.com>
2023-07-15 17:36:53 +0800

    Use organisation name in emails if logo not available

--- resources/views/vendor/mail/html/message.blade.php
+++ resources/views/vendor/mail/html/message.blade.php
@@ -3,7 +3,11 @@
 @slot('header')
 @if(tenant())
 @component('mail::header', ['url' => '//'.tenant('host')])
+@if(tenant('logo_url'))
 <img src="{{ tenant('logo_url') }}" alt="{{ tenant('name') ?? 'Organisation Name' }}">
+@else
+{{ tenant('name') }}
+@endif
 @endcomponent
 @else
 @component('mail::header', ['url' => config('app.url')])
