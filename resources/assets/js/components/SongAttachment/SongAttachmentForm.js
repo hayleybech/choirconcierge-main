c3554b3c3b07fe409e3f8d146910a4a8761d5b92
Hayley Bech <hayleybech@gmail.com>
2023-07-29 18:53:52 +0800

    Add collapsible heading to "add attachment"

--- resources/assets/js/components/SongAttachment/SongAttachmentForm.js
+++ resources/assets/js/components/SongAttachment/SongAttachmentForm.js
@@ -10,6 +10,9 @@ import RadioGroup from "../inputs/RadioGroup";
 import AttachmentType from "../../AttachmentType";
 import TextInput from "../inputs/TextInput";
 import useRoute from "../../hooks/useRoute";
+import CollapseHeader from "../CollapseHeader";
+import { Disclosure } from "@headlessui/react";
+import CollapseTitle from "../CollapseTitle";
 
 const SongAttachmentForm = ({ song }) => {
     const { route } = useRoute();
@@ -29,68 +32,80 @@ const SongAttachmentForm = ({ song }) => {
     }
 
     return (
-        <div className="md:max-w-5xl md:mx-auto px-4 sm:px-6 lg:px-8 pb-8">
-            <FormSection title="Add Attachment">
-                <div className="sm:col-span-6">
+      <Disclosure >
+        {({ open }) => (<>
+          <CollapseHeader>
+            <Disclosure.Button>
+              <CollapseTitle open={open}>Add Attachment</CollapseTitle>
+            </Disclosure.Button>
+          </CollapseHeader>
+
+          <Disclosure.Panel>
+            <div className="md:max-w-5xl md:mx-auto px-4 sm:px-6 lg:px-8 pb-8">
+                <FormSection>
+                  <div className="sm:col-span-6">
                     <RadioGroup
-                        label={<Label label="Attachment Type" />}
-                        options={Object.keys(AttachmentType.types).map(slug => ({
-                            id: slug,
-                            name: AttachmentType.get(slug).title,
-                            textColour: AttachmentType.get(slug).textColour,
-                            colour: AttachmentType.get(slug).textColour,
-                            icon: AttachmentType.get(slug).icon,
-                        }))}
-                        vertical
-                        selected={data.type}
-                        setSelected={value => setData('type', value)}
+                      label={<Label label="Attachment Type" />}
+                      options={Object.keys(AttachmentType.types).map(slug => ({
+                        id: slug,
+                        name: AttachmentType.get(slug).title,
+                        textColour: AttachmentType.get(slug).textColour,
+                        colour: AttachmentType.get(slug).textColour,
+                        icon: AttachmentType.get(slug).icon,
+                      }))}
+                      vertical
+                      selected={data.type}
+                      setSelected={value => setData('type', value)}
                     />
                     {errors.type && <Error>{errors.type}</Error>}
-                </div>
-                {data.type === 'youtube' ? (
-                <>
-                    <div className="sm:col-span-6">
+                  </div>
+                  {data.type === 'youtube' ? (
+                    <>
+                      <div className="sm:col-span-6">
                         <Label label="YouTube URL" forInput="url" />
                         <TextInput
-                            name="url"
-                            value={data.url}
-                            updateFn={value => setData('url', value)}
-                            hasErrors={ !! errors['url'] }
-                            placeholder="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+                          name="url"
+                          value={data.url}
+                          updateFn={value => setData('url', value)}
+                          hasErrors={ !! errors['url'] }
+                          placeholder="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
                         />
                         {errors.url && <Error>{errors.url}</Error>}
-                    </div>
-                    <div className="sm:col-span-6">
+                      </div>
+                      <div className="sm:col-span-6">
                         <Label label="Video description" forInput="title" />
                         <TextInput
-                            name="title"
-                            value={data.title}
-                            updateFn={value => setData('title', value)}
-                            hasErrors={ !! errors['title'] }
-                            placeholder="Never Gonna Give You Up"
+                          name="title"
+                          value={data.title}
+                          updateFn={value => setData('title', value)}
+                          hasErrors={ !! errors['title'] }
+                          placeholder="Never Gonna Give You Up"
                         />
                         {errors.title && <Error>{errors.title}</Error>}
-                    </div>
-                </>
-                ) : (
-                <div className="sm:col-span-6">
-                    <Label label="File Upload" forInput="attachment_uploads" />
-                    <FileInput
+                      </div>
+                    </>
+                  ) : (
+                    <div className="sm:col-span-6">
+                      <Label label="File Upload" forInput="attachment_uploads" />
+                      <FileInput
                         name="attachment_uploads"
                         value={data.attachment_uploads}
                         updateFn={value => setData('attachment_uploads', value)}
                         hasErrors={ !! errors['attachment_uploads'] }
                         multiple
                         vertical
-                    />
-                    {errors.attachment_uploads && <Error>{errors.attachment_uploads}</Error>}
-                </div>
-                )}
-                <div className="">
+                      />
+                      {errors.attachment_uploads && <Error>{errors.attachment_uploads}</Error>}
+                    </div>
+                  )}
+                  <div className="">
                     <Button onClick={submit} variant="primary" size="sm" disabled={processing}><Icon icon="check" />Save</Button>
-                </div>
-            </FormSection>
-        </div>
+                  </div>
+                </FormSection>
+            </div>
+          </Disclosure.Panel>
+        </>)}
+      </Disclosure>
     );
 }
 
