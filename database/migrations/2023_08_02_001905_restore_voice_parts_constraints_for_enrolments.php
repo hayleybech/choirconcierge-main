f6631c86e3e6dc1e01c3b770d6eec1854a48109f
Hayley Bech <hayleybech@gmail.com>
2023-08-02 08:49:24 +0800

    Remove orphaned enrolments before adding voice part constraints

--- database/migrations/2023_08_02_001905_restore_voice_parts_constraints_for_enrolments.php
+++ database/migrations/2023_08_02_001905_restore_voice_parts_constraints_for_enrolments.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Query\Builder;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
@@ -13,6 +14,12 @@ return new class extends Migration
      */
     public function up()
     {
+        DB::table('enrolments')->whereNotExists(function (Builder $query) {
+            $query->select(DB::raw(1))
+                ->from('voice_parts')
+                ->whereColumn('voice_parts.id', 'enrolments.voice_part_id');
+        })->delete();
+
         Schema::table('enrolments', function (Blueprint $table) {
             $table->foreign('voice_part_id')->references('id')->on('voice_parts')->nullOnDelete();
         });
