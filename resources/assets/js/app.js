ed5370a4b2cc1ba7f606adbf40b6958939720bdc
Hayley Bech <hayleybech@gmail.com>
2024-07-07 18:49:17 +0800

    Bump version
    Signed-off-by: Hayley Bech <hayleybech@gmail.com>

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -4,7 +4,7 @@ import { createInertiaApp } from '@inertiajs/react'
 import * as Sentry from '@sentry/react';
 import {Integrations as TracingIntegrations} from "@sentry/tracing";
 
-const VERSION = 'choir-concierge@2024-05-25-a';
+const VERSION = 'choir-concierge@2024-07-07-a';
 
 Sentry.init({
 	dsn: process.env.MIX_SENTRY_DSN,
