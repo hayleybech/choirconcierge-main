13533d4c9f369f7061e25502466eed300585b8ea
Hayley Bech <hayleybech@gmail.com>
2024-10-17 16:44:31 +0800

    Tweak help text

--- resources/assets/js/Pages/Central/Dash/TenantStatsWidget.js
+++ resources/assets/js/Pages/Central/Dash/TenantStatsWidget.js
@@ -25,8 +25,9 @@ const TenantStatsWidget = ({activeTenants, tenantsOnTrial, tenantsTrialExpired, 
 			</div>
 			<div
 				className="flex flex-wrap items-baseline justify-between gap-x-4 gap-y-2 px-4 py-10 sm:px-6 xl:px-8">
-				<dt className="text-sm font-medium leading-6 text-gray-500">Active Members (of active tenants)</dt>
+				<dt className="text-sm font-medium leading-6 text-gray-500">Active Members</dt>
 				<dd className="w-full flex-none text-3xl font-medium leading-10 tracking-tight text-gray-900">{activeMembers}</dd>
+				<div className="text-xs text-gray-400">Active Members of Active Tenants</div>
 			</div>
 		</dl>
 	</Panel>
