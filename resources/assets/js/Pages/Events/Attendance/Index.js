3b5d06bf5ef213feb99b1ca9d7a5002f8095b16e
Hayley Bech <hayleybech@gmail.com>
2025-10-31 11:39:17 +0800

    Simplify current attendance record vs editing

--- resources/assets/js/Pages/Events/Attendance/Index.js
+++ resources/assets/js/Pages/Events/Attendance/Index.js
@@ -13,9 +13,9 @@ import Dialog from '../../../components/Dialog';
 import { usePage } from '@inertiajs/react';
 import QRCode from 'react-qr-code';
 import DateTag from '../../../components/DateTag';
+import ButtonLink from '../../../components/inputs/ButtonLink';
 
 const Index = ({ event, voice_parts, individualCheckInUrl }) => {
-	const [absentReasons, setAbsentReasons] = useState({});
 	const [checkInDialogIsOpen, setCheckInDialogIsOpen] = useState(false);
 
 	const { route } = useRoute();
@@ -149,135 +149,11 @@ const Index = ({ event, voice_parts, individualCheckInUrl }) => {
 							</div>
 							<ul role="list" className="relative z-0 divide-y divide-gray-200">
 								{part.members.map(attendance => (
-									<li key={attendance.member.id} className="bg-white">
-										<div className="relative px-6 py-5 flex flex-col xl:flex-row items-stretch xl:items-center gap-y-3 sm:gap-x-3 hover:bg-gray-50 justify-between">
-											<div className="flex space-x-2 shrink-0">
-												<div className="shrink-0">
-													<img
-														className="h-12 w-12 rounded-lg"
-														src={attendance.member.user.avatar_url}
-														alt={attendance.member.user.name}
-													/>
-												</div>
-												<div className="flex-1 min-w-0">
-													<p className="text-sm font-medium text-gray-900">
-														{attendance.member.user.name}
-													</p>
-													<p className="text-sm">
-														<AttendanceTag
-															label={attendance.label}
-															icon={attendance.icon}
-															colour={attendance.colour}
-														/>
-														{!!attendance.updated_at && (
-															<DateTag
-																label="Updated"
-																date={attendance.updated_at}
-																format="DATETIME_SHORT"
-																className="text-gray-400"
-															/>
-														)}
-														{attendance.response.includes('absent') &&
-															attendance.absent_reason && (
-																<div className="text-gray-500">
-																	Reason for absence: {attendance.absent_reason}
-																</div>
-															)}
-													</p>
-												</div>
-											</div>
-											<div className="shrink-0 flex flex-col sm:flex-row space-y-2 sm:space-y-0 sm:space-x-2 items:stretch sm:items-end">
-												<div className="text-sm text-gray-700 mr-2 mb-2">Mark as:</div>
-												{[
-													{
-														response: 'present',
-														label: 'On Time',
-														icon: 'check',
-														variant: 'success-outline',
-													},
-													{
-														response: 'late',
-														label: 'Late',
-														icon: 'alarm-exclamation',
-														variant: 'warning-outline',
-													},
-													{
-														response: 'late_deemed_absent',
-														label: 'Late (Deemed Absent)',
-														icon: 'times',
-														variant: 'danger-outline',
-													},
-												].map(
-													({ response, label, icon, variant }) =>
-														attendance.response !== response && (
-															<Button
-																href={route('events.attendances.update', {
-																	event,
-																	singer: attendance.member.id,
-																})}
-																method="put"
-																data={{
-																	response: response,
-																	absent_reason: absentReasons[attendance.member.id],
-																}}
-																preserveScroll
-																size="sm"
-																variant={variant}
-																key={response}
-															>
-																<Icon icon={icon} />
-																{label}
-															</Button>
-														)
-												)}
-
-												{!['absent', 'absent_apology'].includes(
-													attendance.response
-												) && (
-													<>
-														<div className="flex flex-col items-stretch gap-0.5 sm:gap-2">
-															<Label
-																label="Reason for absence (Optional)"
-																forInput={`absent_reason_${attendance.member.id}`}
-															/>
-															<TextInput
-																name="absent_reason"
-																id={`absent_reason_${attendance.member.id}`}
-																value={absentReasons[attendance.member.id]}
-																updateFn={value =>
-																	setAbsentReasons({
-																		...absentReasons,
-																		[attendance.member.id]: value,
-																	})
-																}
-																wrapperClasses="grow"
-															/>
-														</div>
-
-														<Button
-															href={route('events.attendances.update', {
-																event,
-																singer: attendance.member.id,
-															})}
-															method="put"
-															data={{
-																response: !!absentReasons[attendance.member.id]
-																	? 'absent_apology'
-																	: 'absent',
-																absent_reason: absentReasons[attendance.member.id],
-															}}
-															preserveScroll
-															size="sm"
-															variant="danger-outline"
-														>
-															<Icon icon="times" />
-															Absent
-														</Button>
-													</>
-												)}
-											</div>
-										</div>
-									</li>
+									<AttendanceRecord
+										key={attendance.member.id}
+										attendance={attendance}
+										event={event}
+									/>
 								))}
 							</ul>
 						</div>
@@ -291,3 +167,131 @@ const Index = ({ event, voice_parts, individualCheckInUrl }) => {
 Index.layout = page => <TenantLayout children={page} />;
 
 export default Index;
+
+const AttendanceRecord = ({ attendance, event }) => {
+	const [absentReason, setAbsentReason] = useState();
+	const [isEditing, setIsEditing] = useState(attendance.response === 'unknown');
+
+	return (
+		<li className="bg-white">
+			<div className="relative px-6 py-5 flex flex-col xl:flex-row items-stretch xl:items-center gap-y-3 sm:gap-x-3 hover:bg-gray-50 justify-between">
+				<div className="flex space-x-2 shrink-0 grow items-center">
+					<div className="shrink-0">
+						<img
+							className="h-12 w-12 rounded-lg"
+							src={attendance.member.user.avatar_url}
+							alt={attendance.member.user.name}
+						/>
+					</div>
+					<div className="shrink-0">
+						<p className="text-sm font-medium text-gray-900">{attendance.member.user.name}</p>
+					</div>
+				</div>
+				{isEditing ? (
+					<div className="shrink-0 flex flex-col sm:flex-row space-y-2 sm:space-y-0 sm:space-x-2 items:stretch sm:items-end">
+						<div className="text-sm text-gray-700 mr-2 mb-2">Mark as:</div>
+						{[
+							{
+								response: 'present',
+								label: 'On Time',
+								icon: 'check',
+								variant: 'success-outline',
+							},
+							{
+								response: 'late',
+								label: 'Late',
+								icon: 'alarm-exclamation',
+								variant: 'warning-outline',
+							},
+							{
+								response: 'late_deemed_absent',
+								label: 'Late (Deemed Absent)',
+								icon: 'times',
+								variant: 'danger-outline',
+							},
+						].map(
+							({ response, label, icon, variant }) =>
+								attendance.response !== response && (
+									<Button
+										href={route('events.attendances.update', {
+											event,
+											singer: attendance.member.id,
+										})}
+										method="put"
+										data={{
+											response: response,
+											absent_reason: absentReason,
+										}}
+										preserveScroll
+										size="sm"
+										variant={variant}
+										key={response}
+									>
+										<Icon icon={icon} />
+										{label}
+									</Button>
+								)
+						)}
+
+						{!['absent', 'absent_apology'].includes(attendance.response) && (
+							<>
+								<div className="flex flex-col items-stretch gap-0.5 sm:gap-2">
+									<Label
+										label="Reason for absence (Optional)"
+										forInput={`absent_reason_${attendance.member.id}`}
+									/>
+									<TextInput
+										name="absent_reason"
+										id={`absent_reason_${attendance.member.id}`}
+										value={absentReason}
+										updateFn={value => setAbsentReason(value)}
+										wrapperClasses="grow"
+									/>
+								</div>
+
+								<Button
+									href={route('events.attendances.update', {
+										event,
+										singer: attendance.member.id,
+									})}
+									method="put"
+									data={{
+										response: !!absentReason ? 'absent_apology' : 'absent',
+										absent_reason: absentReason,
+									}}
+									preserveScroll
+									size="sm"
+									variant="danger-outline"
+								>
+									<Icon icon="times" />
+									Absent
+								</Button>
+							</>
+						)}
+					</div>
+				) : (
+					<div className="flex flex-col">
+						<div className="flex gap-2 items-center">
+							<AttendanceTag label={attendance.label} icon={attendance.icon} colour={attendance.colour} />
+							<Button variant="primary" size="xs" onClick={() => setIsEditing(true)}>
+								<Icon icon="edit" />
+								Change
+							</Button>
+						</div>
+						{!!attendance.updated_at && (
+							<DateTag
+								label="Updated"
+								date={attendance.updated_at}
+								format="DATETIME_SHORT"
+								className="text-sm text-gray-400"
+							/>
+						)}
+						{attendance.response.includes('absent') && attendance.absent_reason && (
+							<div className="text-sm text-gray-500">Reason for absence: {attendance.absent_reason}</div>
+						)}
+					</div>
+				)}
+			</div>
+		</li>
+	);
+};
