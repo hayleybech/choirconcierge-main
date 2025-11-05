26029298f46424bbadb73654c2c28c8b46bdf1a7
Hayley Bech <hayleybech@gmail.com>
2025-11-05 11:59:07 +0800

    Fix song title truncation in audio player

--- resources/assets/js/components/Audio/GlobalTrackPlayer.js
+++ resources/assets/js/components/Audio/GlobalTrackPlayer.js
@@ -15,18 +15,18 @@ const GlobalTrackPlayer = ({ songTitle, songId, fileName, close }) => {
     const { togglePlayPause, ready, loading, playing, stop } = useAudioPlayer();
 
     return (
-        <div className="relative z-10 shrink-0 flex h-auto sm:h-12 bg-white border-t border-gray-300 flex flex-col sm:flex-row items-center justify-between py-2 px-2 sm:pl-6">
-            <div className="flex flex-row items-start">
-                <Button variant="clear" size="xs" disabled className="invisible sm:hidden">
+        <div className="relative z-10 shrink-0 h-auto sm:h-12 bg-white border-t border-gray-300 flex flex-col sm:flex-row items-center justify-between py-2 px-2 sm:pl-6">
+            <div className="flex flex-row items-start w-full min-w-0">
+                <Button variant="clear" size="xs" disabled className="invisible sm:hidden shrink-0">
                     <Icon icon="times" />
                 </Button>
 
-                <div className="flex flex-col sm:flex-row items-center text-center sm:text-left mb-2 sm:mb-0 sm:mr-8">
-                    <Link href={route('songs.show', {song: songId})} className="text-sm text-purple-800 sm:mr-4">{songTitle}</Link>
-                    <span className="text-gray-600 text-xs truncate">{fileName}</span>
+                <div className="flex flex-col sm:flex-row items-center text-center sm:text-left mb-2 sm:mb-0 sm:mr-8 min-w-0">
+                    <Link href={route('songs.show', {song: songId})} className="text-sm text-purple-800 sm:mr-4 w-full truncate">{songTitle}</Link>
+                    <span className="text-gray-600 text-xs truncate w-full">{fileName}</span>
                 </div>
 
-                <Button variant="clear" size="xs" onClick={() => {stop(); close();}} className="sm:hidden">
+                <Button variant="clear" size="xs" onClick={() => {stop(); close();}} className="sm:hidden shrink-0">
                     <Icon icon="times" />
                 </Button>
             </div>
