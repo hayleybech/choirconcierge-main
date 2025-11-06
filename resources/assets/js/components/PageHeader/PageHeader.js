394a60e375c914d509cbaf4eb29fcdbcf22fb9e4
Hayley Bech <hayleybech@gmail.com>
2025-11-06 11:11:44 +0800

    Create PageHeading component

--- resources/assets/js/components/PageHeader/PageHeader.js
+++ resources/assets/js/components/PageHeader/PageHeader.js
@@ -4,6 +4,7 @@ import Button from '../inputs/Button';
 import Icon from '../Icon';
 import ActionMenuItem from '../ActionMenu/ActionMenuItem';
 import ActionMenu from '../ActionMenu/ActionMenu';
+import { PageHeading } from './PageHeading';
 
 const PageHeader = ({ title, image, icon, meta, breadcrumbs, actions = [], optionsVariant }) => {
 	const filteredActions = actions.filter(action => !!action);
@@ -15,10 +16,11 @@ const PageHeader = ({ title, image, icon, meta, breadcrumbs, actions = [], optio
 					{image && <img src={image} alt={title} className="h-32 rounded-md mb-3 lg:mb-0 mr-6" />}
 					<div className="flex-1 min-w-0">
 						<Breadcrumbs breadcrumbs={breadcrumbs} />
-						<h2 className="mt-2 text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate">
+						<PageHeading>
 							{icon && <Icon icon={icon} type="solid" className="mr-2" />}
 							<span>{title}</span>
-						</h2>
+						</PageHeading>
+
 						<div className="mt-1 flex flex-col sm:flex-row sm:flex-wrap sm:mt-2 gap-2 sm:gap-6 text-sm sm:items-center text-gray-500">
 							{meta}
 						</div>
---
+++ resources/assets/js/components/PageHeader/PageHeading.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const PageHeading = ({ children }) => (
+	<h2 className="mt-2 text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate">{children}</h2>
+);
