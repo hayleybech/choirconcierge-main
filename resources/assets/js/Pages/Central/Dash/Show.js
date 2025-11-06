7d232cde12297b43499a4ba919bc06d9ce5cd439
Hayley Bech <hayleybech@gmail.com>
2025-11-06 16:24:16 +0800

    Add description to central dash

--- resources/assets/js/Pages/Central/Dash/Show.js
+++ resources/assets/js/Pages/Central/Dash/Show.js
@@ -11,7 +11,10 @@ const Show = ({ events, songs, tenantStats }) => (
         <AppHead title="Dashboard" />
         <div className="py-6">
             <div className="mx-auto px-4 sm:px-6 lg:px-16">
-                <h1 className="text-2xl font-semibold text-gray-900 mb-8">Dashboard</h1>
+                <div className="mb-8">
+                    <h1 className="text-2xl font-semibold text-gray-900 mb-2">Central Dashboard</h1>
+                    <p className="text-sm text-gray-700">This combined dashboard shows upcoming events and songs for all of your choirs. Click one of your choirs below to go to individual Dashboards. </p>
+                </div>
 
                 <div className="grid gap-y-6 gap-x-4 grid-cols-1 sm:grid-cols-2 xl:grid-cols-3">
                     {!!tenantStats && (
