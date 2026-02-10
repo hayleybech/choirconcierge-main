b48865a4cafca67a71f84f2702349597b072462c
Hayley Bech <hayleybech@gmail.com>
2026-02-10 15:14:32 +0800

    Make domain tag lighter

--- resources/assets/js/components/DomainTag.js
+++ resources/assets/js/components/DomainTag.js
@@ -1,5 +1,5 @@
 const DomainTag = ({children}) => (
-	<code className="px-1 py-0.5 rounded-sm bg-gray-200 text-xs font-bold">
+	<code className="px-1 py-0.5 rounded-sm bg-gray-50 text-xs font-bold">
 		{children}
 	</code>
 );
