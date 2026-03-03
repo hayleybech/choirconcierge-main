11fa203ffc12c2b920c94d871836acd28996daab
Hayley Bech <hayleybech@gmail.com>
2026-03-03 18:20:30 +0800

    Reverse sort order when switching between upcoming/past event filters

--- resources/assets/js/components/Event/EventFilters.js
+++ resources/assets/js/components/Event/EventFilters.js
@@ -6,40 +6,53 @@ import RadioGroup from "../inputs/RadioGroup";
 import Filters from "../Filters";
 
 const EventFilters = ({ eventTypes, form }) => (
-    <Filters
-        routeName="events.index"
-        form={form}
-        render={(data, setData) => (<>
-            <div>
-                <Label label="Title" forInput="title" />
-                <TextInput name="title" value={data.title} updateFn={value => setData('title', value)} />
-            </div>
+	<Filters
+		routeName="events.index"
+		form={form}
+		render={(data, setData) => (
+			<>
+				<div>
+					<Label label="Title" forInput="title" />
+					<TextInput name="title" value={data.title} updateFn={value => setData('title', value)} />
+				</div>
 
-            <fieldset className="">
-                <legend className="text-sm font-medium text-gray-700">Type</legend>
-                <CheckboxGroup
-                    name="type.id"
-                    options={eventTypes.map((type) => ({ id: type.id, name: type.title }))}
-                    value={data['type.id']}
-                    updateFn={value => setData('type.id', value)}
-                />
-            </fieldset>
+				<fieldset className="">
+					<legend className="text-sm font-medium text-gray-700">Type</legend>
+					<CheckboxGroup
+						name="type.id"
+						options={eventTypes.map(type => ({ id: type.id, name: type.title }))}
+						value={data['type.id']}
+						updateFn={value => setData('type.id', value)}
+					/>
+				</fieldset>
 
-            <div>
-                <RadioGroup
-                    label={<Label label="Date" />}
-                    options={[
-                        { id: 'all', name: 'All' },
-                        { id: 'upcoming', name: 'Upcoming' },
-                        { id: 'past', name: 'Past' },
-                    ]}
-                    selected={data.date}
-                    setSelected={value => setData('date', value)}
-                    vertical
-                />
-            </div>
-        </>)}
-    />
+				<div>
+					<RadioGroup
+						label={<Label label="Date" />}
+						options={[
+							{ id: 'all', name: 'All' },
+							{ id: 'upcoming', name: 'Upcoming' },
+							{ id: 'past', name: 'Past' },
+						]}
+						selected={data.date}
+						setSelected={value => {
+							setData('date', value);
+
+							if (value === 'upcoming') {
+								setData('sort', 'start_date');
+								setData('sortDir', 'asc')
+							}
+							if (value === 'past') {
+								setData('sort', 'start_date');
+								setData('sortDir', 'desc');
+							}
+						}}
+						vertical
+					/>
+				</div>
+			</>
+		)}
+	/>
 );
 
 export default EventFilters;
