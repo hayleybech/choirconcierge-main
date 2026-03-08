c7b3ad2daa59b3b1704fd116e042b64d79a3d2ca
Hayley Bech <hayleybech@gmail.com>
2026-03-08 13:36:16 +0800

    Validate group slugs to alphadash

--- app/Http/Requests/UserGroupRequest.php
+++ app/Http/Requests/UserGroupRequest.php
@@ -30,6 +30,7 @@ class UserGroupRequest extends FormRequest
             'title' => ['required', 'max:255'],
             'slug' => [
                 'required',
+                'alpha_dash',
                 Rule::unique('user_groups')
                     ->where('tenant_id', tenant('id'))
                     ->ignore($this->group->id ?? ''),
