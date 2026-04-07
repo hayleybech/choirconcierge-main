68503079cfa48514a9fafc6221205815f65d2c8c
Hayley Bech <hayleybech@gmail.com>
2026-04-07 16:41:55 +0800

    Uninstall spark

--- app/Providers/SparkServiceProvider.php
+++ app/Providers/SparkServiceProvider.php
@@ -6,8 +6,8 @@ use App\Models\Tenant;
 use Illuminate\Http\Request;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Validation\ValidationException;
-use Spark\Plan;
-use Spark\Spark;
+//use Spark\Plan;
+//use Spark\Spark;
 
 class SparkServiceProvider extends ServiceProvider
 {
@@ -16,33 +16,33 @@ class SparkServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Spark::billable(Tenant::class)->resolve(function (Request $request) {
-            return $request->tenant;
-        });
+//        Spark::billable(Tenant::class)->resolve(function (Request $request) {
+//            return $request->tenant;
+//        });
 
-        Spark::billable(Tenant::class)->authorize(function (Tenant $billable, Request $request) {
-            return $billable->id !== 'demo'
-                && $request->user() && (
-                    $request->user()->is($billable->billingUser)
-                    || $request->user()
-                        ->memberships()
-                        ->firstWhere('tenant_id', $billable->id)
-                        ?->hasRole('Admin')
-                    || $request->user()
-                        ->memberships()
-                        ->firstWhere('tenant_id', $billable->id)
-                        ?->hasRole('Accounts Team')
-                );
-        });
+//        Spark::billable(Tenant::class)->authorize(function (Tenant $billable, Request $request) {
+//            return $billable->id !== 'demo'
+//                && $request->user() && (
+//                    $request->user()->is($billable->billingUser)
+//                    || $request->user()
+//                        ->memberships()
+//                        ->firstWhere('tenant_id', $billable->id)
+//                        ?->hasRole('Admin')
+//                    || $request->user()
+//                        ->memberships()
+//                        ->firstWhere('tenant_id', $billable->id)
+//                        ?->hasRole('Accounts Team')
+//                );
+//        });
 
-        Spark::billable(Tenant::class)->checkPlanEligibility(function (Tenant $billable, Plan $plan) {
-            if($plan->options['activeUserQuota'] !== null
-	            && $billable->billingStatus['activeUserQuota']['activeUserCount'] > $plan->options['activeUserQuota']
-            ) {
-                throw ValidationException::withMessages([
-                     'plan' => 'You have too many active users for the selected plan.',
-                 ]);
-            }
-        });
+//        Spark::billable(Tenant::class)->checkPlanEligibility(function (Tenant $billable, Plan $plan) {
+//            if($plan->options['activeUserQuota'] !== null
+//	            && $billable->billingStatus['activeUserQuota']['activeUserCount'] > $plan->options['activeUserQuota']
+//            ) {
+//                throw ValidationException::withMessages([
+//                     'plan' => 'You have too many active users for the selected plan.',
+//                 ]);
+//            }
+//        });
     }
 }
