44de75fe5a9fe3633e5fa5ebaef629a19612f3df
Hayley Bech <hayleybech@gmail.com>
2026-08-11 16:49:49 +0800

    Allow existing users to create tenants

--- resources/assets/js/Pages/Central/Dash/ChoirsListWidget.js
+++ resources/assets/js/Pages/Central/Dash/ChoirsListWidget.js
@@ -1,66 +1,74 @@
 import React from 'react';
 import Panel, { PanelTitle } from '../../../components/Panel';
-import TableMobile, {TableMobileItem} from "../../../components/TableMobile";
-import {usePage} from "@inertiajs/react";
-import useRoute from "../../../hooks/useRoute";
-import Button from "../../../components/inputs/Button";
+import TableMobile, { TableMobileItem } from '../../../components/TableMobile';
+import { usePage } from '@inertiajs/react';
+import useRoute from '../../../hooks/useRoute';
+import Button from '../../../components/inputs/Button';
+import Icon from '../../../components/Icon';
 
 const ChoirsListWidget = () => {
-    const { route } = useRoute();
-    const { userChoirs: choirs, user } = usePage().props;
+	const { route } = useRoute();
+	const { userChoirs: choirs, user } = usePage().props;
 
-    return (
-        <Panel header={<PanelTitle>Your Choirs</PanelTitle>} noPadding>
-            {choirs.length > 0 ? (
-                <TableMobile>
-                    {choirs.map((choir) => (
-                        <TableMobileItem url={route('dash', {tenant: choir.id})} key={choir.id}>
-                            <div className="flex items-center justify-between w-full gap-4">
-                                {choir.logo_url
-                                  ? <img src={choir.logo_url} alt={choir.name} className="max-h-10 w-auto shrink min-w-0" />
-                                  : <span className="text-purple-800">{choir.name}</span>
-                                }
+	return (
+		<Panel header={<div className="flex justify-between items-center">
+			<PanelTitle>Your Choirs</PanelTitle>
+			<Button href={route('central.tenants.create')} variant="secondary" size="xs"><Icon icon="plus" />Setup New Choir</Button>
+		</div>} noPadding>
+			{choirs.length > 0 ? (
+				<TableMobile>
+					{choirs.map(choir => (
+						<TableMobileItem url={route('dash', { tenant: choir.id })} key={choir.id}>
+							<div className="flex items-center justify-between w-full gap-4">
+								{choir.logo_url ? (
+									<img
+										src={choir.logo_url}
+										alt={choir.name}
+										className="max-h-10 w-auto shrink min-w-0"
+									/>
+								) : (
+									<span className="text-purple-800">{choir.name}</span>
+								)}
 
-                                {choirs.length > 1 && (
-                                    <>
-                                    {user.default_tenant_id !== choir.id
-                                        ? (
-                                        <Button
-                                            variant="secondary"
-                                            size="xs"
-                                            href={route('central.default-dash.update', {default_dash: choir.id})}
-                                            method="put"
-                                            className="mr-2 shrink-0"
-                                        >
-                                            Set as default
-                                        </Button>
-                                        )
-                                        : (
-                                        <div className="shrink-0">
-                                            <span className="text-gray-600 text-sm mr-2">Default Choir</span>
-                                            <Button
-                                                variant="danger-outline"
-                                                size="xs"
-                                                href={route('central.default-dash.destroy', {default_dash: choir.id})}
-                                                method="delete"
-                                                className="mr-2"
-                                            >
-                                                Unset default
-                                            </Button>
-                                        </div>
-                                        )
-                                    }
-                                    </>
-                                )}
-                            </div>
-                        </TableMobileItem>
-                    ))}
-                </TableMobile>
-            ) : (
-                <p className="px-4 py-4 sm:px-6">It looks like you haven't joined a choir.</p>
-            )}
-        </Panel>
-    );
-}
+								{choirs.length > 1 && (
+									<>
+										{user.default_tenant_id !== choir.id ? (
+											<Button
+												variant="secondary"
+												size="xs"
+												href={route('central.default-dash.update', { default_dash: choir.id })}
+												method="put"
+												className="mr-2 shrink-0"
+											>
+												Set as default
+											</Button>
+										) : (
+											<div className="shrink-0">
+												<span className="text-gray-600 text-sm mr-2">Default Choir</span>
+												<Button
+													variant="danger-outline"
+													size="xs"
+													href={route('central.default-dash.destroy', {
+														default_dash: choir.id,
+													})}
+													method="delete"
+													className="mr-2"
+												>
+													Unset default
+												</Button>
+											</div>
+										)}
+									</>
+								)}
+							</div>
+						</TableMobileItem>
+					))}
+				</TableMobile>
+			) : (
+				<p className="px-4 py-4 sm:px-6">It looks like you haven't joined a choir.</p>
+			)}
+		</Panel>
+	);
+};
 
 export default ChoirsListWidget;
