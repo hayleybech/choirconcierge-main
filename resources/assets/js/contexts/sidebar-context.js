0fdb53ab98f75f07f29253c63dfe306b28cb83e6
Hayley Bech <hayleybech@gmail.com>
2026-09-15 18:26:37 +0800

    Open RN sidebar from drawer button

--- resources/assets/js/contexts/sidebar-context.js
+++ resources/assets/js/contexts/sidebar-context.js
@@ -1,10 +1,25 @@
 import React, { useContext } from 'react';
+import { sendMessageToRN } from '../lib/reactNative';
 
 export const SidebarContext = React.createContext(null);
 
-export const SidebarProvider = ({ setSidebarOpen, children }) => (
-	<SidebarContext.Provider value={{ setSidebarOpen }}>{children}</SidebarContext.Provider>
-);
+export const SidebarProvider = ({ setSidebarOpen, children }) => {
+	const handleSidebarOpen = open => {
+		if (open === true && window.ReactNativeWebView) {
+			sendMessageToRN({ action: 'openSidebar' });
+
+			return;
+		}
+
+		setSidebarOpen(open);
+	};
+
+	return (
+		<SidebarContext.Provider value={{ setSidebarOpen: handleSidebarOpen }}>
+			{children}
+		</SidebarContext.Provider>
+	);
+};
 
 export const useSidebar = () => {
 	const context = useContext(SidebarContext);
